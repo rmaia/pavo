484a9af398f7b64a59d409d58d1dd38def7ca3c0
rmaia <rm72@zips.uakron.edu>
2012-10-12 23:27:35 -0400

    edited agg fxn

--- R/agg.R
+++ R/agg.R
@@ -1,18 +1,50 @@
 # Function to calculate average of columns given a column index (by)
 # Returns a matrix with column names as the by's and rows as averaged values for each wl
 # Input column 1 should be wavelengths
-agg <- function(x, by = stop("by is needed"), FUN = stop("FUN is needed")) {
-wl <- x[,1]
-x <- x[,-1]
-by0 <- by  # retain original values
-by <- as.numeric(factor(by))  # allow for numeric, character data
-dat <- matrix(data = NA, nrow = nrow(x), ncol = length(unique(by)))
+
+# RM tip: Always provide useful defaults. If you don't want to provide one 
+#     (i.e. there is no meaningful default), leave arg empty. Default is to return 
+#     error. But if there's an implemented default (i.e. for FUN), use it.
+
+agg <- function(x, by, FUN = mean) {
+
+#BEGIN RM EDIT
+# check: user may have removed 'wl' function already.
+# (data.frame doesn't allow duplicate names anymore, so this should work)
+
+wl_index <- which(names(x)=='wl')
+
+if(length(wl_index>0)){
+	wl <- x[,wl_index]
+	y <- x[,-wl_index]
+	}else{
+		y <- x
+		}
+
+# check: does x have the same number of columns as the by vector?
+
+if(dim(y)[2]!=length(by)) 
+stop(paste('\n',dQuote(deparse(substitute(by))),'is not of same length as columns in',dQuote(deparse(substitute(x)))))
+
+#END RM EDIT
+
+# retain original values
+by0 <- by
+# allow for numeric, character data
+by <- as.numeric(factor(by))  
+
+dat <- matrix(data = NA, nrow = nrow(y), ncol = length(unique(by)))
+
 for (i in seq(along = unique(by))) {
-	if (is.null(dim(x[,by==i]))) {
-	dat[,i] <- apply(cbind(x[,by==i], x[,by==i]), 1, FUN)  # apply can't avg single columns
-	}
-	else dat[,i] <- apply(x[,by==i], 1, FUN)
+# apply can't avg single columns
+	if (is.null(dim(y[,by==i]))) {
+	   dat[,i] <- apply(cbind(y[,by==i], y[,by==i]), 1, FUN)
+	}else{
+		dat[,i] <- apply(y[,by==i], 1, FUN)
+		}
 }
+
 colnames(dat) <- unique(by0)
-as.data.frame(cbind(wl=wl, dat))
+data.frame(cbind(wl=wl, dat))
 }
+
