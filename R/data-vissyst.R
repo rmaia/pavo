91441ecaffc075085dc4ec054c8ef0869f45afc1
rmaia <rm72@zips.uakron.edu>
2012-10-14 16:39:57 -0400

    added visual system data to package structure

---
+++ R/data-vissyst.R
@@ -0,0 +1,8 @@
+#' Animal visual systems data
+#'
+#' @name vissyst
+#' @docType data
+#' @author Rafael Maia \email{rm72@@zips.uakron.edu}
+#' @references \url{data_blah.com}
+#' @keywords data
+NULL
