0cae55dfe7e74d2197e4a3447c1412c8796a6fb5
rmaia <rm72@zips.uakron.edu>
2012-10-19 10:30:27 -0400

    ttplot working (using spheres)

--- R_temp/tetraplotnew_test.R
+++ R_temp/tetraplotnew_test.R
@@ -1,5 +1,5 @@
 
-tetraplot<- function(tcsres, size=5, col='black', new=T, hspin=T, vspin=F) {
+tetraplot<- function(tcsres, size=0.025, col='black', new=T, hspin=T, vspin=F, floor=T) {
 
 if(new)
    open3d()
@@ -10,54 +10,64 @@ ttv=pavo::ttvertex
 
 plot3d(unlist(ttv[c('xu','xs','xm','xl')]),
 		unlist(ttv[c('yu','ys','ym','yl')]),
-		unlist(ttv[c('zu','zs','zm','zl')]),
-		size=5, box=F, axes=F, xlab='',ylab='',zlab='',
+		unlist(ttv[c('zu','zs','zm','zl')]), type='s', lit=F,
+		radius=0.02, box=F, axes=F, xlab='',ylab='',zlab='',
 		col=c('purple','blue','green','red'))
 
-segments3d(ttv[c('xu','xs')], ttv[c('yu','ys')], ttv[c('zu','zs')], alpha=0.5)
-segments3d(ttv[c('xu','xm')], ttv[c('yu','ym')], ttv[c('zu','zm')], alpha=0.5)
-segments3d(ttv[c('xu','xl')], ttv[c('yu','yl')], ttv[c('zu','zl')], alpha=0.5)
-segments3d(ttv[c('xs','xm')], ttv[c('ys','ym')], ttv[c('zs','zm')], alpha=0.5)
-segments3d(ttv[c('xs','xl')], ttv[c('ys','yl')], ttv[c('zs','zl')], alpha=0.5)
-segments3d(ttv[c('xl','xm')], ttv[c('yl','ym')], ttv[c('zl','zm')], alpha=0.5)
 
-points3d(0,0,0,col='grey', pch=22,size=2)
+segments3d(ttv[c('xu','xs')], ttv[c('yu','ys')], ttv[c('zu','zs')], color='lightgrey')
+segments3d(ttv[c('xu','xm')], ttv[c('yu','ym')], ttv[c('zu','zm')], color='lightgrey')
+segments3d(ttv[c('xu','xl')], ttv[c('yu','yl')], ttv[c('zu','zl')], color='lightgrey')
+segments3d(ttv[c('xs','xm')], ttv[c('ys','ym')], ttv[c('zs','zm')], color='lightgrey')
+segments3d(ttv[c('xs','xl')], ttv[c('ys','yl')], ttv[c('zs','zl')], color='lightgrey')
+segments3d(ttv[c('xl','xm')], ttv[c('yl','ym')], ttv[c('zl','zm')], color='lightgrey')
 
-points3d(tcsres$tcs[,c('x','y','z')], size=size, color=col)
+spheres3d(0,0,0,col='grey', radius=0.01, lit=F)
 
-# #   vertices <- c( 
-     # -0.7, -0.5, -0.3, 1.0,
-      # 0.7, -0.5, -0.3, 1.0,
-      # 0.7,  0.8, -0.3, 1.0,
-     # -0.7,  0.8, -0.3, 1.0
-  # )
-  # indices <- c( 1, 2, 3, 4 )
+spheres3d(tcsres$tcs[,c('x','y','z')], radius=size, color=col, lit=F)
+
+if(floor){
+  vertices <- c( 
+      -0.7, -0.5, -0.3, 1.0,
+       0.7, -0.5, -0.3, 1.0,
+       0.7,  1, -0.3, 1.0,
+      -0.7,  1, -0.3, 1.0
+  				)
+  indices <- c( 1, 2, 3, 4 )
   
-# wire3d( qmesh3d(vertices,indices) )
-
+ wire3d( qmesh3d(vertices,indices), lit=F )
+	}
+	
 if(hspin)
-   play3d(spin3d(axis=c(0,0,1), rpm=10), duration=5)
+   play3d(spin3d(axis=c(0,0,1), rpm=20), duration=3)
 
 if(vspin)
-   play3d(spin3d(axis=c(1,0,0), rpm=10), duration=5)
+   play3d(spin3d(axis=c(1,0,0), rpm=20), duration=3)
 
 }
 
 
 
 require(pavo)
+require(rgl)
 
-a=getspec('~/github/pavo/examplespec')
-b=getspec('~/github/pavo/examplespec','ttt')
-a=data.frame(cbind(a,b))
-rm(b)
-vis.a=vismodel(a)
-tcs.a=tcs(vis.a)
+ a=getspec('~/github/pavo/examplespec')
+ b=getspec('~/github/pavo/examplespec','ttt')
+ a=data.frame(cbind(a,b))
+ rm(b)
+ vis.a=vismodel(a)
+ tcs.a=tcs(vis.a)
 
 
 
-tetraplot(tcs.a, new=F)
+tetraplot(tcs.a, new=T,hspin=F)
 
+#voltest<-t(convhulln(tcs.a$tcs[,c('x','y','z')]))
+
+#coord <- tcs.a$tcs[,c('x','y','z')]
+
+#rgl.triangles(coord[voltest,1],coord[voltest,2],coord[voltest,3],alpha=.2)
 
 #saving options seem very limited
+rgl.postscript('testplot.pdf',fmt='pdf')
 rgl.snapshot('testplot.png')
