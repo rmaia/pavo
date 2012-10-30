e3591254d5621d0f4d40f34ddcf62f64b6aa658d
Pierre-Paul Bitton <bittonp@uwindsor.ca>
2012-10-30 12:54:53 -0400

    added todo and some debugging

--- R_temp/Plot spec curves_Function.R
+++ R_temp/Plot spec curves_Function.R
@@ -1,25 +1,35 @@
-plot.spec.curves <- function (filename, specreps) {
+## TODO (Pierre): Allow function to plot 1 curve at a time
+## TODO (Pierre): Put nicer colours!
+## TODO (Pierre): Arg to change number of figures per plot
+## TODO (Pierre): Test
+## TODO (Pierre): Error handling
+## TODO (Pierre): Documentation
 
-x<- read.table (filename, sep=",", header=T)
+plot.spec.curves <- function (specdata, specreps=0) {
+
+if (specreps == 0) stop ("Need to include number of spectral curves per figure")
+
 
 plotnumber <- 1
-nplots <- ((ncol(x)-1)/specreps)
+nplots <- ((ncol(specdata)-1)/specreps)
 par(mfrow=c(3,4),ask=TRUE)
 
 for (i in 1:nplots){
 
-	bloc <-x[,(((plotnumber-1)*specreps)+2):((plotnumber*specreps)+1)]
+	bloc <-specdata[,(((plotnumber-1)*specreps)+2):((plotnumber*specreps)+1)]
 	specs <- c(1:ncol(bloc))
 	yaxismin <-min(bloc)
 	yaxismax <-max(bloc)
-	plot(x[,1],bloc[,1],cex=0.1,ylim=c(yaxismin,yaxismax+5),col=1,xlab="Wavelength (nm)",ylab="% Reflectance")
+	plot(specdata[,1],bloc[,1],cex=0.1,ylim=c(yaxismin,yaxismax+5),col=1,xlab="Wavelength (nm)",ylab="% Reflectance")
 	legend (280,yaxismax+6,legend=names(bloc),cex=0.7,text.col = specs,bty="n", xjust=0)
 
 nextplot = 2
 		while (nextplot < ncol(bloc)+1) { 						
-			lines (x[,1],bloc[,nextplot],cex=0.1, col = nextplot)
+			lines (specdata[,1],bloc[,nextplot],cex=0.1, col = nextplot)
 			nextplot<- nextplot+1}
 
 plotnumber=plotnumber+1}
 }
+
+plot.spec.curves(specdata)
 
