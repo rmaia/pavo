e73f71844076e19f453a9926e936102e784e2d87
Pierre-Paul Bitton <bittonp@uwindsor.ca>
2012-10-30 16:14:41 -0400

    Changed to nicer colors

--- R_temp/Plot spec curves_Function.R
+++ R_temp/Plot spec curves_Function.R
@@ -1,4 +1,4 @@
-## TODO (Pierre): Put nicer colours!
+## TODO (Pierre): Fix the small color bug (first plot different)
 ## TODO (Pierre): Arg to change number of figures per plot
 ## TODO (Pierre): Test
 ## TODO (Pierre): Error handling
@@ -6,9 +6,7 @@
 
 plot.spec.curves <- function (specdata, specreps=0) {
 
-if (specreps == 0) stop ("Need to include number of spectral curves per figure")
-
-
+if (specreps <= 0) stop ("Need to include number of spectral curves per figure")
 
 nplots <- ((ncol(specdata)-1)/specreps)
 par(mfrow=c(3,4),ask=TRUE)
@@ -27,18 +25,17 @@ for (i in 1:nplots){
 	}
 	
 	if (specreps > 1){
-	specs <- c(1:ncol(bloc))
-	plot(specdata[,1],bloc[,1],cex=0.1,ylim=c(yaxismin,yaxismax+5),col=1,xlab="Wavelength (nm)",ylab="% Reflectance")
-	legend (280,yaxismax+6,legend=names(bloc),cex=0.7,bty="n", xjust=0,text.col = specs)
+	legcolor <- palette(rainbow(ncol(bloc)))
+	plot(specdata[,1],bloc[,1],cex=0.1,ylim=c(yaxismin,yaxismax+5),col=legcolor[1],xlab="Wavelength (nm)",ylab="% Reflectance")
+	legend (280,yaxismax+6,legend=names(bloc),cex=0.7,bty="n", xjust=0, text.col = legcolor)
 	
-
 	nextplot = 2
 		while (nextplot < ncol(bloc)+1) { 						
-			lines (specdata[,1],bloc[,nextplot],cex=0.1, col = nextplot)
+			lines (specdata[,1],bloc[,nextplot],cex=0.1, col = legcolor[nextplot])
 			nextplot<- nextplot+1}
-}
-}
+		}
+	}
 }
 
-plot.spec.curves(specdata)
+
 
