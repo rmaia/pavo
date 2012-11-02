4e90ce0a341da42504cdc8fe131369d8b21bf604
Pierre-Paul Bitton <bittonp@uwindsor.ca>
2012-11-01 22:15:24 -0400

    Added documentation

--- R_temp/Plot spec curves_Function.R
+++ R_temp/Plot spec curves_Function.R
@@ -1,11 +1,23 @@
-## TODO (Pierre): Fix the small color bug (first plot different)
+## TODO (Pierre): Fix the colors
 ## TODO (Pierre): Test
 ## TODO (Pierre): Error handling
 ## TODO (Pierre): Documentation
 
-plot.spec.curves <- function (specdata, specreps=0) {
+#' Plot spectral curves
+#'
+#' Plots spectral curves in the same graph to rapidly compare groups of spectra
+#'
+#' @param specdata(required) a data frame that has wavelength range in the first 
+#' column and spectral measurements in the remaining columns. Number of plots 
+#' presented per page depends on the number of curves per graph
+#' @param specreps(required) number of spectra to include in each graph
+#' @return Spectral curve plots
+#' @export
 
-if (specreps <= 0) stop ("Need to include number of spectral curves per figure")
+plot.spec.curves <- function (specdata, specreps=1) {
+
+if (specreps == 0) stop ("Number of spectral curves cannot be 0")
+if (specreps < 0) stop ("specreps: No negative values allowed")
 
 nplots <- ((ncol(specdata)-1)/specreps)
 par(mfrow=c(3,4),ask=TRUE)
@@ -17,7 +29,11 @@ if (specreps > 12) par(mfrow=c(1,1))
 for (i in 1:nplots){
 	leg <- names(specdata[,2:dim(specdata)[2]])
 	if (specreps == 1) bloc <- specdata[, i+1]
-	if (specreps > 1) bloc <-specdata[,(((i-1)*specreps)+2):((i*specreps)+1)]
+	
+	if (specreps > 1) {
+	bloc <-specdata[,(((i-1)*specreps)+2):((i*specreps)+1)]
+	legcolor <- palette(rainbow(n=ncol(bloc), start = 0.7, end = 0.1))
+	}
 	yaxismin <-min(bloc)
 	yaxismax <-max(bloc)
 	
@@ -27,8 +43,7 @@ for (i in 1:nplots){
 	}
 	
 	if (specreps > 1){
-	legcolor <- palette(rainbow(ncol(bloc)))
-	plot(specdata[,1],bloc[,1],cex=0.1,ylim=c(yaxismin,yaxismax+5),col=legcolor[1],xlab="Wavelength (nm)",ylab="% Reflectance")
+		plot(specdata[,1],bloc[,1],cex=0.1,ylim=c(yaxismin,yaxismax+5),col=legcolor[1],xlab="Wavelength (nm)",ylab="% Reflectance")
 	legend (280,yaxismax+6,legend=names(bloc),cex=0.7,bty="n", xjust=0, text.col = legcolor)
 	
 	nextplot = 2
