63b26f0ffe1e2666c4fa97b886643e70ac0a9a07
Chad Eliason <cme1980@gmail.com>
2012-11-13 15:15:00 -0500

    changed procspec, plot_rspec
    add binning, centering opts;  updated plot_rspec to work with single
    spectra

--- R_temp/plot_rspec.r
+++ R_temp/plot_rspec.r
@@ -1,37 +1,66 @@
 # Pavo function to plot groups of spectra
 # p is 'picked' spectra; can be a numeric vector or factor (e.g., sex=="male")
-# for heatplots, by is a vector describing numeric values across which spectra are varying
+# for heatmaps, 'by' is a vector describing numeric values across which spectra are varying
+# 'n' is number of bins to interpolate 'varying' over
 
 # TODO: 
-# 1. add way to plot mean+/-sd plots
-# 2. add interpolating argument for heatmap
-# 3. add additional title, axes, color arguments
+# 1. add additional title, axes, color arguments
 
-plot.rspec <- function(specs, p = NULL, type = "overlay", by = NULL, col=heat.colors(25)) {
-	type <- match.arg(type, c("heatmap", "overlay", "stack"))
-	wl <- specs[, 1]
+plot.rspec <- function(specs, p = NULL, type = c("overlay", "stack", "heatmap"), 
+											 varying = NULL, by = NULL, col = heat.colors(25), rows = 2, 
+											 n = 100) {
+
+	old.par <- par(no.readonly = TRUE)  # all par settings that could be set
+
+	type <- match.arg(type)
+
+	wl_index <- which(names(data)=='wl')
+	if (length(wl_index > 0)) {
+		wl <- specs[, wl_index]
+		specs <- as.data.frame(specs[, -wl_index])
+	} else {
+		specs <- as.data.frame(specs)
+		}
+
+	# subset based on indexing vector
 	if (is.logical(p)) p <- which(p=="TRUE")
-	if (is.null(p)) p <- 2:ncol(specs)
-	specs <- specs[, p]
+	if (is.null(p)) p <- 1:ncol(specs)
+	specs <- as.data.frame(specs[, p])
 
 	if (type=="heatmap") {
-		if (is.null(by))
-			by <- 1:ncol(specs)
-		dat <- sapply(1:nrow(specs), function(z){approx(x = by, y = specs[z, ], n = 100)$y})
-		image(x = wl, y = approx(by, n = 100)$y, z = t(dat), col = col)
+		if (is.null(varying))
+			varying <- 1:ncol(specs)
+		dat <- sapply(1:nrow(specs), function(z){approx(x = varying, y = specs[z, ], 
+								  n = n)$y})
+		image(x = wl, y = approx(varying, n = n)$y, z = t(dat), col = col,
+					xlab = "Wavelength (nm)")
 	}
-	
+
 	if (type=="overlay") {
-		plot(specs[, 1]~wl, type = 'l', ylim = c(min(specs), max(specs)))
-		for (i in 2:ncol(specs)) lines(specs[, i]~wl)
+		plot(specs[, 1]~wl, type = 'l', ylim = c(min(specs), max(specs)), 
+				 xlab = "Wavelength (nm)", ylab = "Reflectance (%)")
+		if (ncol(specs)>1) for (i in 2:ncol(specs)) lines(specs[, i]~wl)
 	}
-	
+
 	if (type=="stack") {
 		specs2 <- sapply(1:ncol(specs), function(z){specs[, z] - min(specs[, z])})
 		ym <- apply(specs2, 2, max)  
-		plot(specs2[, 1], ylim=c(0, sum(ym)), type='l')
+		plot(specs2[, 1], ylim=c(0, sum(ym)), type='l', xlab = "Wavelength (nm)")
 		for (i in 2:ncol(specs)) {
 			lines(specs2[, i] + cumsum(ym)[i - 1])
 		}
 	}
+
+	par(old.par)
+
 }
+
+# Testing zone for new plotting function
+
+#plot(rspecs, p=c(2,14,9), type="h", by=c(10,20,30))
+#plot(witu, p=2:7, type="h", varying=seq(10,60,by=10))
+#plot(rspecs, p=c(2,14,9,12,13,19), type="o", by=c(10,20,30))
+#plot(rspecs, p=2:4, type='s')
+#rspecs2 <- procspec(rspecs, f="stretch")
+#plot(rspecs2, p=2:8, type='o')
+#plot(rspecs, by=spp, type='g', rows=3)
--- R_temp/procspec.R
+++ R_temp/procspec.R
@@ -1,49 +1,84 @@
-# input spectra with rows as specs and columns as wavelengths
-# fun options = min, max, stretch, sum, [bin?], [diffs?]
+# input an rspec object or data frame w/spectra in columns and wavelengths in rows
+# options = min, max, stretch, sum, bin, center (subtract avg. from all specs)
+
 # TODO
 # 1. add way to handle negatives
-# 2. implement switch, match.arg
- 
-procspec <- function(Y, f=NULL, sm=FALSE, spar = .65, span = .15, method="spline") {
 
-	wl <- Y[, 1]
-	Y <- Y[, -1]
-	
-	# smoothing
-	if (sm==F) Y2 <- Y
-	else {
-		if (method=="spline")
-			Y2 <- sapply(row.names(Y), function(z){smooth.spline(x=wl, y=Y[z,], spar=spar)$y})
-		if (method=="loess")
-			Y2 <- sapply(row.names(Y), function(z){loess.smooth(x=wl, y=Y[z,], span=span, 
-									 degree=2, family="gaussian", evaluation = length(wl))$y})
+procspec <- function(specs, opt = c('none', 'smooth', 'maximum', 'minimum', 'stretch', 
+										 'bin', 'fix', 'sum', 'center'), method = c('loess', 'spline'), 
+										 spar = .65, span = .15, bins = 20) {
+
+	wl_index <- which(names(specs)=='wl')
+	if (length(wl_index > 0)){
+		wl <- specs[, wl_index]
+		specs <- specs[, -wl_index]
+		} else {
+			specs <- specs
+			}
+
+	opt <- match.arg(opt, several.ok = TRUE)  	# 'fix' = how to handle negative values
+	method <- match.arg(method)
+
+	if (any(opt=='none')) {
+		print('No relevant processing option entered. Returning raw values') 
+		specs <- as.data.frame(cbind(wl, specs))
+		class(specs) <- c('rspec', 'data.frame')
+		return(specs)
 	}
 	
-	# normalization
-	if (is.null(f)) Y2 <- Y
-	if (f=="min") Y2 <- sapply(1:ncol(Y2), function(z)Y2[,z] - min(Y2[,z]))
-	if (f=="max") Y2 <- sapply(1:ncol(Y2), function(z)Y2[,z] - max(Y2[,z]))
-	if (f=="sum") Y2 <- sapply(1:ncol(Y2), function(z)Y2[,z] / sum(Y2[,z]))
-	if (f=="stretch") {
-		Y2 <- sapply(1:ncol(Y2), function(z)Y2[,z] - min(Y2[,z]))
-		Y2 <- sapply(1:ncol(Y2), function(z)Y2[,z] / max(Y2[,z]))
+	if (any(opt=='smooth')&method=='spline')
+		specs <- sapply(names(specs), function(z){smooth.spline(x = wl, y = specs[, z], 
+										spar = spar)$y})
+
+	if (any(opt=='smooth')&method=='loess')
+		specs <- sapply(names(specs), function(z){loess.smooth(x = wl, y = specs[, z], 
+										span = span, degree = 2, family = "gaussian", 
+										evaluation = length(wl))$y})
+
+	if (any(opt=='minimum'))
+		specs <- sapply(1:ncol(specs), function(z)specs[, z] - min(specs[, z]))
+
+	if (any(opt=='maximum'))
+		specs <- sapply(1:ncol(specs), function(z)specs[, z] / max(specs[, z]))
+
+	if (any(opt=='sum'))
+		specs <- sapply(1:ncol(specs), function(z)specs[, z] / sum(specs[, z]))
+
+	if (any(opt=='stretch')) {
+		specs <- sapply(1:ncol(specs), function(z)specs[, z] - min(specs[, z]))
+		specs <- sapply(1:ncol(specs), function(z)specs[, z] / max(specs[, z]))
 	}
+
+	if (any(opt=='center'))
+		specs <- sapply(1:ncol(specs), function(z)specs[, z] - mean(specs[, z]))
+
+#	if (any(opt=='fix'))
+#		specs <- ...
+		# option 1 = set all negatives to zero
+		# option 2 = add absolute val of most neg value to all spectra
+		# option 3 = set negatives to NAs, then smooth?
+
+	# Calculate medians according to # of bins specified for use in PCA
+	# Method follows Cuthill et al. (1999)
+	if (any(opt=='bin')) {
+		bw <- floor(length(wl)/bins)
+		wl_bin <- seq(head(wl,1), tail(wl,1), by=bw)
+		wl_ind <- match(wl_bin, wl)
+		specs <- sapply(1:(length(wl_ind)-1), function(z) 
+									  apply(specs[wl_ind[z]:(wl_ind[z]+bw), ], 2, median))
+	}
+
+	if (any(opt=='bin'))
+		{specs <- as.data.frame(cbind(wl_bin[-length(wl_bin)], t(specs)))}
+		else {specs <- as.data.frame(cbind(wl, specs))}
 	
-	Y2 <- as.data.frame(cbind(wl, Y2))
+	class(specs) <- c('rspec', 'data.frame')
 	
-	class(Y2) <- c('rspec', 'data.frame')
-	
-	Y2
+	specs
 
 }
 
-
 # testing zone
+#tmp <- procspec(rspecs)
 #tmp <- procspec(rspecs, f="stretch")
 #plot(tmp, p=8:15, type='h')
-
-# NOTES ON BETTER WAY TO DO THIS FUNCTION:
-	# proc = c('smooth', 'normalize')
-	# method = c('loess', 'spline', 'max', 'min')
-	# proc <- match.arg(proc, c('smooth', 'normalize')
-	# method <- match.arg(method, c('loess', 'spline', 'maximum', 'minimum', 'stretch'))
