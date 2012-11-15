5183991eb10a7146b59c205098837fa7aa530d80
Pierre-Paul Bitton <bittonp@uwindsor.ca>
2012-11-15 14:52:56 -0500

    minor doc edits

--- R_temp/plotsmooth.R
+++ R_temp/plotsmooth.R
@@ -3,10 +3,12 @@
 #' Plots curves with various levels of loess smoothing to help determine what
 #' loess parameters are best for the data
 #'
-#' @param all.specs(required) A data frame of format \code{rspec}
-#' @param minsmooth The minimum f value of the loess function to visualize (defaults to 0.02)
+#' @param all.specs(required) A data frame, possibly of class \code{rspec}, which
+#' contains a column containing a wavelength range , named 'wl', and spectra data in 
+#' remaining columns.
+#' @param minsmooth The minimum f value of the loess function to visualize (defaults to 0.05)
 #' @param maxsmooth The maximum f value of the loess function to visualize (defaults to 0.20)
-#' @param gap The number of curves to display on the same plot (defaults to 5)
+#' @param curves The number of curves to display on the same plot (defaults to 5)
 #' @param specnum The number of spectral curves, from the data frame, to visualize (defaults to ALL)
 #' @return Series of plot with curves processed with varying level of loess smoothing
 #' export
