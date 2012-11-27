a6cc2c9d5bdbce8e744f8809df49ea1dccf300c8
Pierre-Paul Bitton <bittonp@uwindsor.ca>
2012-11-27 11:33:12 -0500

    explorespec standardized and minor doc edit

--- R/explorespec.R
+++ R/explorespec.R
@@ -3,35 +3,39 @@
 #' Plots one or multiple spectral curves in the same graph to rapidly 
 #' compare groups of spectra.
 #'
-#' @param specdata(required) a data frame, possibly an object of class \code{rspec}
+#' @param rspecdata(required) a data frame, possibly an object of class \code{rspec}
 #' that has wavelength range in the first column, named 'wl', and spectral measurements in the 
 #' remaining columns. 
 #' @param specreps number of spectra to include in each graph (defaults to 1)
-#' @param lwd Width of the lines displayed on the plots (defaults to 2)
+#' @param lwd width of the lines displayed on the plots (defaults to 2)
+#' @param scale defines how the y-axis should be scaled. \code{'free'}: panels can vary in
+#' the range of the y-axis; \code{'equal'}: all panels have the y-axis with the same range.
 #' @return Spectral curve plots
 #' @note Number of plots presented per page depends on the number of curves per graph.
 #' @export
 #' @examples \dontrun{
 #' data(sicalis)
 #' explorespec(sicalis,3)}
-#' @author Pierre-Paul Bitton \email{bittonp@@uwindsor.ca}
+#' @author Pierre-Paul Bitton \email{bittonp@uwindsor.ca}
 
 
 
-explorespec <- function (specdata, specreps=1, lwd=2) {
+explorespec <- function (rspecdata, specreps=1, lwd=2, scale=c('free','equal')) {
 
 
 if (specreps <= 0) stop ("Invalid specreps value")
 
-wl_index <- which(names(specdata)=='wl')
-wl <- specdata[,wl_index]
-specdata <- specdata[,-wl_index]
-leg2 <- names(specdata)
-if ((dim(specdata)[2]/specreps) != round((dim(specdata)[2]/specreps))){
-  warning("specreps is not a factor of the number of column in specdata")
+wl_index <- which(names(rspecdata)=='wl')
+wl <- rspecdata[,wl_index]
+rspecdata <- rspecdata[,-wl_index]
+leg2 <- names(rspecdata)
+if ((dim(rspecdata)[2]/specreps) != round((dim(rspecdata)[2]/specreps))){
+  warning("specreps is not a factor of the number of column in rspecdata")
 }
 
-nplots <- ceiling(dim(specdata)[2]/specreps)
+scale <- match.arg(scale)
+
+nplots <- ceiling(dim(rspecdata)[2]/specreps)
 
 if(specreps <=4){
   par(mfrow=c(3,4),ask=TRUE)
@@ -64,12 +68,17 @@ col_list <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
 
 for (i in 1:nplots){
   if (specreps == 1) {
-  	bloc <- data.frame(specdata[i])
+  	bloc <- data.frame(rspecdata[i])
   	col_list <- 'black' }else{
-      bloc <- specdata[,(((i-1)*specreps)+1):min(i*specreps,dim(specdata)[2])]
+      bloc <- rspecdata[,(((i-1)*specreps)+1):min(i*specreps,dim(rspecdata)[2])]
   	  }
 
-  yaxislims = c(min(bloc),max(bloc))*yaxismult
+  if(scale=='free')
+    yaxislims <- c(min(bloc), max(bloc))*yaxismult
+
+  if(scale=='equal')
+    yaxislims <- c(min(rspecdata), max(rspecdata))*yaxismult
+
   leg <- names(bloc)
   
   if (!is.null(dim(bloc))){
