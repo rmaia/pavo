8ffa10eb6c0d030983ac764a32f5ef3b3c8300d0
rmaia <rm72@zips.uakron.edu>
2012-11-28 09:11:11 -0500

    added projpoints fxn

--- R/projplot.R
+++ R/projplot.R
@@ -21,7 +21,7 @@
 #' @references Stoddard, M. C., & Prum, R. O. (2008). Evolution of avian plumage color in a tetrahedral color space: A phylogenetic analysis of new world buntings. The American Naturalist, 171(6), 755-776.
 #' @references Endler, J. A., Westcott, D., Madden, J., & Robson, T. (2005). Animal visual systems and the evolution of color patterns: Sensory processing illuminates signal evolution. Evolution, 59(8), 1795-1818.
 
-projplot=function(tcsres, ...)
+projplot <- function(tcsres, ...)
 {
 
 # no longer tcs object
@@ -32,18 +32,18 @@ projplot=function(tcsres, ...)
     # }
 
 
-points.theta=tcsres[,'h.theta']
-points.phi=tcsres[,'h.phi']
+points.theta <- tcsres[,'h.theta']
+points.phi <- tcsres[,'h.phi']
 
-n=length(points.theta)
+n <- length(points.theta)
 
 #Edges of the tetrahedron, adjusted
-vert.theta=c(-3.1415, 3.1415, -1.047198, 1.047198, -2.617994)
-vert.phi=c(-0.3398369, -0.3398369, -0.3398369, -0.3398369,  1.5707963)
+vert.theta <- c(-3.1415, 3.1415, -1.047198, 1.047198, -2.617994)
+vert.phi <- c(-0.3398369, -0.3398369, -0.3398369, -0.3398369,  1.5707963)
 
 #Edges of the figure
-edge.theta=c(-pi,-pi,pi,pi)
-edge.phi=c(-pi/2,pi/2,-pi/2,pi/2)
+edge.theta <- c(-pi,-pi,pi,pi)
+edge.phi <- c(-pi/2,pi/2,-pi/2,pi/2)
 
 #adjust points
 
@@ -53,25 +53,25 @@ points.theta <- ifelse(points.theta>= -0.5235988,
                        
 
 # radians to degrees
-coords.theta = c(edge.theta,vert.theta,points.theta)*180/pi
-coords.phi = c(edge.phi,vert.phi,points.phi)*180/pi
+coords.theta <- c(edge.theta,vert.theta,points.theta)*180/pi
+coords.phi <- c(edge.phi,vert.phi,points.phi)*180/pi
 
 #map projection coordinates
 
-mp=mapproject(coords.theta, coords.phi, projection="mollweide")
+mp <- mapproject(coords.theta, coords.phi, projection="mollweide")
 
-mp.v.theta=mp$x[1:9]
-mp.v.phi=mp$y[1:9]
+mp.v.theta <- mp$x[1:9]
+mp.v.phi <- mp$y[1:9]
 
-mp.p.theta=mp$x[-c(1:9)]
-mp.p.phi=mp$y[-c(1:9)]
+mp.p.theta <- mp$x[-c(1:9)]
+mp.p.phi <- mp$y[-c(1:9)]
 
 #plot
 
-cu=t(col2rgb('#984EA3'))/255
-cs=t(col2rgb('#377EB8'))/255
-cm=t(col2rgb('#4DAF4A'))/255
-cl=t(col2rgb('#E41A1C'))/255
+cu <- t(col2rgb('#984EA3'))/255
+cs <- t(col2rgb('#377EB8'))/255
+cm <- t(col2rgb('#4DAF4A'))/255
+cl <- t(col2rgb('#E41A1C'))/255
 
 par(mar=c(0,0,0,0))
 plot(0,0,axes=F,xlab='',ylab='', type='n',frame.plot=F, xlim=c(-2,2), ylim=c(-1,1))
@@ -80,5 +80,57 @@ map.grid(c(-180,180,-90,90),labels=F,col='grey')
 
 points(mp.v.phi~mp.v.theta,pch=20,cex=1.5,col=c(rep('grey',4),rgb(cl),rgb(cl),rgb(cm),rgb(cs),rgb(cu)))
 
-points(mp.p.phi~mp.p.theta,pch=20, ...)
+points(mp.p.phi~mp.p.theta, ...)
 }
+
+
+
+
+projpoints <- function(tcsres, ...)
+{
+
+# no longer tcs object
+# if(class(tcsres)=='tcs'){
+  # dat <- tcsres$tcs	
+  # }else{
+    # dat <- tcsres
+    # }
+
+
+points.theta <- tcsres[,'h.theta']
+points.phi <- tcsres[,'h.phi']
+
+n <- length(points.theta)
+
+#Edges of the tetrahedron, adjusted
+vert.theta <- c(-3.1415, 3.1415, -1.047198, 1.047198, -2.617994)
+vert.phi <- c(-0.3398369, -0.3398369, -0.3398369, -0.3398369,  1.5707963)
+
+#Edges of the figure
+edge.theta <- c(-pi,-pi,pi,pi)
+edge.phi <- c(-pi/2,pi/2,-pi/2,pi/2)
+
+#adjust points
+
+points.theta <- ifelse(points.theta>= -0.5235988,
+                       points.theta-(150/180*pi),
+                       points.theta+(210/180*pi))
+                       
+
+# radians to degrees
+coords.theta <- c(edge.theta,vert.theta,points.theta)*180/pi
+coords.phi <- c(edge.phi,vert.phi,points.phi)*180/pi
+
+#map projection coordinates
+
+mp <- mapproject(coords.theta, coords.phi, projection="mollweide")
+
+mp.v.theta <- mp$x[1:9]
+mp.v.phi <- mp$y[1:9]
+
+mp.p.theta <- mp$x[-c(1:9)]
+mp.p.phi <- mp$y[-c(1:9)]
+
+points(mp.p.phi~mp.p.theta, ...)
+}
+
