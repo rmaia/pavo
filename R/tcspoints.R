d62defe28528d1cd27e4be2c50f991fd1f20fbdb
rmaia <rm72@zips.uakron.edu>
2012-12-04 16:02:49 -0500

    renamed tcsplot

---
+++ R/tcspoints.R
@@ -0,0 +1,22 @@
+#' Plot a Tetrahedral Color Space
+#'
+#' \code{tcspoints} plots points in a tetrahedral color space
+#'
+#' @return \code{tcsplot} creates 3D points in a tetrahedral color space plot produced by \code{tcsplot}
+#' using functions of the package \code{rgl}, based on openGL capabilities.
+#'
+#' @rdname tcsplot
+#'
+#' @export
+
+tcspoints<- function(tcsdata, size=0.02, col='black'){
+
+# if(class(tcsdata)=='tcs'){
+  # dat <- tcsdata$tcs  
+  # }else{
+    # dat <- tcsdata
+    # }
+
+
+spheres3d(tcsdata[,c('x','y','z')], radius=size, color=col, lit=F)
+}
