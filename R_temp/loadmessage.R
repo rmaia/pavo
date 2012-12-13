0bfdaf296135d643a025906afcecf914541974e3
rmaia <rm72@zips.uakron.edu>
2012-12-13 00:20:57 -0500

    manuscript

--- R_temp/loadmessage.R
+++ R_temp/loadmessage.R
@@ -1,7 +1,6 @@
 .onAttach <- function(lib, pkg)  {
     packageStartupMessage('When citing results from pavo, refer to original sources:
-
-trichromatic summary variables: Montgomerie 2006
+tristimulus summary variables: Montgomerie 2006
 photon catch model: Vorobyev & Osorio 1998, Vorobyev et al. 1998
 tetracolorspace model and volume overlap: Stoddard & Prum 2008, Stoddard & Stevens 2011
 package implementation: Maia et al. Submitted (see citation())
