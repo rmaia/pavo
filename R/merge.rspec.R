29e0cafff5acb7da7a0274a163b5f571ecd8caf4
Chad Eliason <cmestuff@gmail.com>
2013-01-10 14:48:53 -0500

    add merge.rspec FXN

---
+++ R/merge.rspec.R
@@ -0,0 +1,28 @@
+#' Merge two rspec objects
+#'
+#' Merges two \code{rspec} or \code{data.frame} objects into a single \code{rspec} object
+#'
+#' @param x,y (required) two data frames (or \code{rspec} objects) to merge
+#' @param ... additional class arguments
+#' @return an object of class \code{rspec} for use with \code{pavo} functions
+#' @export merge.rspec
+#' @examples \dontrun{
+#'
+#' # Load and split dataset into 2 sections
+#' data(teal)
+#' teal1 <- teal[, c(1, 3:5)]
+#' teal2 <- teal[, c(1, 2, 6:12)]
+#' teal.mer <- merge(teal1, teal2, by='wl')
+#' head(teal.mer)
+#' par(mfrow=c(1, 2))
+#' plot(teal.mer)
+#' plot(teal)
+#'
+#' @author Chad Eliason \email{cme16@@zips.uakron.edu}
+
+merge.rspec <- function(x, y, ...) {
+  if (!(is.rspec(x)&&is.rspec(y))) {stop("Invalid rspec objects")}
+  res <- merge.data.frame(x, y, ...)
+  class(res) <- c("rspec", "data.frame")
+  res
+}
