89ebb741db0018e794fced8825f958b8be7f78ae
Chad Eliason <chadeliason@wireless-10-145-119-1.public.utexas.edu>
2014-11-21 12:00:43 -0600

    fix aggspec, add aggplot legend option

--- R/aggspec.R
+++ R/aggspec.R
@@ -40,97 +40,97 @@
 #     error. But if there's an implemented default (i.e. for FUN), use it.
 
 aggspec <- function(rspecdata, by = NULL, FUN = mean, trim = TRUE, ...) {
-
-#BEGIN RM EDIT
-# check: user may have removed 'wl' function already.
-# (data.frame doesn't allow duplicate names anymore, so this should work)
-
-wl_index <- which(names(rspecdata)=='wl')
-
-if (length(wl_index>0)){
-	wl <- rspecdata[, wl_index]
-	y <- rspecdata[, -wl_index]
-	} else {
-		  y <- rspecdata
-		}
-
-if (is.null(by)) {
-  dat <- apply(y, 1, FUN, ...)
-  res <- data.frame(cbind(wl=wl, dat))
-  class(res) <- c('rspec','data.frame')
-  return(res)
-}
-
-if (is.numeric(by))
+  
+  #BEGIN RM EDIT
+  # check: user may have removed 'wl' function already.
+  # (data.frame doesn't allow duplicate names anymore, so this should work)
+  
+  wl_index <- which(names(rspecdata)=='wl')
+  
+  if (length(wl_index>0)){
+    wl <- rspecdata[, wl_index]
+    y <- rspecdata[, -wl_index]
+  } else {
+    y <- rspecdata
+  }
+  
+  if (is.null(by)) {
+    dat <- apply(y, 1, FUN, ...)
+    res <- data.frame(cbind(wl=wl, dat))
+    class(res) <- c('rspec','data.frame')
+    return(res)
+  }
+  
+  if (is.numeric(by))
     if (ncol(y) %% by != 0)
-        stop('by not a multiple of number of spectra')
-
-#BEGIN RM EDIT 2
-# check if the by argument has a 'wl' entry (e.g. if names were obtained through
-# regex conditions on the original spec names) and remove it
-
-if (length(which(by=='wl'))!=0)
-  by <- by[-which(by=='wl')]
-
-#END RM EDIT 2
-
-# Handle when 'by' is a list of factors
-if (is.list(by)) {
-  wl_id <- sapply(1:length(by), function(x) which(by[[x]]=='wl'))  # extract wl columns
-  # remove 'wl' column from each vector in list
+      stop('by not a multiple of number of spectra')
+  
+  #BEGIN RM EDIT 2
+  # check if the by argument has a 'wl' entry (e.g. if names were obtained through
+  # regex conditions on the original spec names) and remove it
+  
+  if (length(which(by=='wl'))!=0)
+    by <- by[-which(by=='wl')]
+  
+  #END RM EDIT 2
+  
+  # Handle when 'by' is a list of factors
+  if (is.list(by)) {
+    wl_id <- sapply(1:length(by), function(x) which(by[[x]]=='wl'))  # extract wl columns
+    # remove 'wl' column from each vector in list
     if (any(sapply(wl_id, length)!=0)) {
       id <- which(sapply(wl_id, length)!=0)
       by[id] <- lapply(by[id], "[", -unlist(wl_id)[id])
     }
-  # check that wl column is the same for all vectors
+    # check that wl column is the same for all vectors
     if (length(unique(wl_id))==1) {
       by <- do.call('paste', c(by, sep='.'))
     } else {
-          stop("mismatch in column names of input vectors")
-      }
-}
-
-# retain original 'by' values
-by0 <- by
-
-#BEGIN RM EDIT 1
-# Allow for means of every "by" data, if "by" is a single number
-# i.e. if by=3, average every 3 consecutive data of "data"
-if (length(by)==1){
-	by0 <- names(y)[seq(1, length(names(y)), by = by)]
-	by <- rep(1:(length(y) / by), each = by)
-}
-#END RM EDIT 1
-
-#BEGIN RM EDIT 3
-# check: does data have the same number of columns as the by vector?
-
-if (dim(y)[2]!=length(by)) 
-stop(paste('\n',dQuote(deparse(substitute(by))),'is not of same length as columns in',dQuote(deparse(substitute(data)))))
-
-#END RM EDIT 3
-
-# Add ability to aggregate based on multiple vectors (given a list as input)
-# TODO: add that list can be an input in roxygen doc 
-
-by <- factor(by)  # is this necessary?
-
-dat <- sapply(unique(by), function(z){apply(y[which(by==z)], 1, FUN, ...)})
-
-colnames(dat) <- unique(by0)
-
-if(trim)
-  colnames(dat) <- gsub('[\\. | \\_ | \\-][0-9]*$', '', colnames(dat))
-
-res <- data.frame(cbind(wl=wl, dat))
-
-class(res) <- c('rspec','data.frame')
-
-res
-
-# This would return list of spectral data and data.frame with metadata:
+      stop("mismatch in column names of input vectors")
+    }
+  }
+  
+  # retain original 'by' values
+  by0 <- by
+  
+  #BEGIN RM EDIT 1
+  # Allow for means of every "by" data, if "by" is a single number
+  # i.e. if by=3, average every 3 consecutive data of "data"
+  if (length(by)==1){
+    by0 <- names(y)[seq(1, length(names(y)), by = by)]
+    by <- rep(1:(length(y) / by), each = by)
+  }
+  #END RM EDIT 1
+  
+  #BEGIN RM EDIT 3
+  # check: does data have the same number of columns as the by vector?
+  
+  if (dim(y)[2]!=length(by)) 
+    stop(paste('\n',dQuote(deparse(substitute(by))),'is not of same length as columns in',dQuote(deparse(substitute(data)))))
+  
+  #END RM EDIT 3
+  
+  # Add ability to aggregate based on multiple vectors (given a list as input)
+  # TODO: add that list can be an input in roxygen doc 
+  
+  by <- factor(by)  # is this necessary?
+  
+  dat <- sapply(levels(by), function(z){apply(y[which(by==z)], 1, FUN, ...)})
+  
+  colnames(dat) <- levels(by)
+  
+  if(trim)
+    colnames(dat) <- gsub('[\\. | \\_ | \\-][0-9]*$', '', colnames(dat))
+  
+  res <- data.frame(cbind(wl=wl, dat))
+  
+  class(res) <- c('rspec','data.frame')
+  
+  res
+  
+  # This would return list of spectral data and data.frame with metadata:
   # set <- strsplit(names(res), split='\\.')
   # out <- as.data.frame(do.call("rbind", set))
   # list(res, out)
-
+  
 }
