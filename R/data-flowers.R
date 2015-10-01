04da2b34d19fbff090d00a0e285e04bdd15112b4
thomased <thomas.white026@gmail.com>
2015-10-01 11:24:50 +1000

    Add floral reflectance data for bee/fly space examples

---
+++ R/data-flowers.R
@@ -0,0 +1,11 @@
+#' Reflectane spectra from a suite of native Australian flowers, collected
+#' around Cairns, Queensland.
+#' 
+#' @name flowers
+#' @docType data 
+#' @author Thomas White \email{thomas.white026@@gmail.com}
+#' @keywords data
+#' @name flowers
+#' @description dataset containing reflectance measurements from 36 native 
+#' Australian angiosperm species, indicated by column names. 
+NULL
