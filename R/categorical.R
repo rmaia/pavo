bf947f1d40469978d660d9f244a7b8a5d3f13cad
thomased <thomas.white026@gmail.com>
2015-11-17 11:14:07 +1100

    Rough in categorical fly model

---
+++ R/categorical.R
@@ -0,0 +1,102 @@
+#' Categorical fly-visual model
+#' 
+#' Applies the fly categorical colour vision model of Troje (1993). 
+#' 
+#' @param vismodeldata (required) quantum catch color data. Can be either the result
+#' from \code{\link{vismodel}} or independently calculated data (in the form of a data frame
+#' with four columns representing a tetrachromatic dipteran viewer).
+#' 
+#' @return Object of class \code{colorspace} consisting of the following columns:
+#' @return \code{R7p, R7y, R8p, R8y}: the quantum catch data used to
+#' calculate the remaining variables.
+#' @return \code{x, y}: cartesian coordinates in the categorical colour space.
+#' @return \code{category}: fly-colour category. One of \code{p-y-}, \code{p-y+}, \code{p+y-}, \code{p+y+}.
+#' @export
+#' @examples
+#' \dontrun{
+#' data(flowers)
+#' vis.flowers <- vismodel(flowers, qcatch = 'Qi', visual = 'bluetit', relative = TRUE, scale = 10000)
+#' cat.flowers <- categorical(vis.flowers)
+#' }
+#' @author Thomas White \email{thomas.white026@@gmail.com}
+#' @references Troje N. (1993). Spectral categories in the learning behaviour
+#' of blowflies. Zeitschrift fur Naturforschung C, 48, 96-96.
+
+categorical <- function(vismodeldata){
+  
+dat <- vismodeldata
+  
+# if object is vismodel:
+if('vismodel' %in% attr(dat, 'class')){
+  
+  # check if tetrachromat
+  if(attr(dat, 'conenumb') < 4)
+    stop('vismodel input is not tetrachromatic')
+  
+  if(attr(dat, 'conenumb') > 4)
+    warning('vismodel input is not tetrachromatic, considering first four receptors only')
+  
+  # check if relative
+  if(!attr(dat, 'relative')){
+    dat <- dat[,c('u','s','m','l')]/rowSums(dat[,c('u','s','m','l')])
+    class(dat) <- class(vismodeldata)
+    warning("Quantum catch are not relative, and have been transformed")
+  }
+  
+}
+
+# if not, check if it has more (or less) than 4 columns
+
+if(!('vismodel' %in% attr(dat, 'class'))){
+  
+  if(ncol(dat) < 4)
+    stop('Input data is not a ',  dQuote('vismodel'), ' object and has fewer than four columns')	
+  if(ncol(dat) == 4)
+    warning('Input data is not a ', dQuote('vismodel'), ' object; treating columns as standardized quantum catch for ', dQuote('u'),', ',  dQuote('s'),', ',  dQuote('m'),', and ', dQuote('l'), ' receptors, respectively')
+  
+  if(ncol(dat) > 4)
+    warning('Input data is not a ', dQuote('vismodel'), ' object *and* has more than four columns; treating the first four columns as standardized quantum catch for ', dQuote('u'),', ',  dQuote('s'),', ',  dQuote('m'),', and ', dQuote('l'), ' receptors, respectively')
+  
+  dat <- dat[, 1:4]
+  
+  if(round(sum(rowSums(dat/apply(dat,1,sum)))) != dim(dat)[1]){
+    dat <- dat/apply(dat, 1, sum)
+    warning('Quantum catch are not relative, and have been divided by their sum')
+  }
+}
+
+
+R7p <- dat[, 1]
+R7y <- dat[, 2]
+R8p <- dat[, 3]
+R8y <- dat[, 4]
+  
+# x & y coordinates
+x <- R7p - R8p
+y <- R7y - R8y
+
+# Colour category calculator (surely a better way?)
+cat <- function(object){
+  if(object$x > 0 && object$y > 0)
+    return('p+y+')
+  if(object$x < 0 && object$y > 0)
+    return('p-y+')
+  if(object$x < 0 && object$y < 0)
+    return('p-y-')
+  if(object$x > 0 && object$y < 0)
+    return('p+y-')
+}
+
+res.p <- data.frame(R7p, R7y, R8p, R8y, x, y, row.names = rownames(dat))
+
+res.p$category <- sapply(1:nrow(res.p), function(x) cat(res.p[x,]))
+
+res <- res.p
+
+class(res) <- c('colorspace', 'data.frame')
+
+attr(res, 'conenumb') <- 4
+attr(res, 'clrsp') <- 'categorical'
+
+res
+}
