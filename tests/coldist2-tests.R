a17b6ea71917fc56a2fa0b74d13c06c67bfec26a
rmaia <rm72@zips.uakron.edu>
2016-10-28 09:44:43 -0400

    just some tests before pulling

--- tests/coldist2-tests.R
+++ tests/coldist2-tests.R
@@ -1,30 +1,43 @@
 require(pavo)
 data(sicalis)
 aa <- vismodel(sicalis, visual=sensmodel(c(400,600)), rel=F, scale=10000000)
-attr(aa, 'conenumb') <- 2 # when using pavo release
+#attr(aa, 'conenumb') <- 2 # when using pavo release
 
-#plot(
-all.equal(
-coldist(aa, vis='di', achro=F)$dS,
+colspace(aa)
+
+plot(
+#all.equal(
+coldist(aa,  achro=F, weber.ref='n2')$dS,
 coldist2(aa, n=c(1,2), weber.ref=2, achro=F, noise='quantum')$dS
-)
+#)
+); abline(0,1)
 
 data(sicalis)
 aa <- vismodel(sicalis, visual=sensmodel(c(400,500,600)), rel=F, scale=10000000)
-attr(aa, 'conenumb') <- 3 # when using pavo release
+#attr(aa, 'conenumb') <- 3 # when using pavo release
 
-#plot(
-all.equal(
-coldist(aa, vis='tri', achro=F)$dS,
+plot(
+#all.equal(
+coldist(aa, achro=F, weber.ref='n3')$dS,
 coldist2(aa, n=c(1,2,2), weber.ref=3, achro=F, noise='quantum')$dS
-)
+#)
+); abline(0,1)
 
 data(sicalis)
 aa <- vismodel(sicalis, visual=sensmodel(c(300,400,500,600)), rel=F, scale=10000000)
-attr(aa, 'conenumb') <- 4 # when using pavo release
+#attr(aa, 'conenumb') <- 4 # when using pavo release
 
-#plot(
-all.equal(
-coldist(aa, vis='tetra', achro=F)$dS,
+plot(
+#all.equal(
+coldist(aa, achro=F)$dS,
 coldist2(aa, n=c(1,2,2,4), weber.ref=4, achro=F, noise='quantum')$dS
-)
+#)
+); abline(0,1)
+
+
+require(pavo)
+packageVersion('pavo') # 0.99
+data(sicalis)
+aa <- vismodel(sicalis, visual=sensmodel(c(400,600)), rel=F, scale=10000000)
+colspace(aa)
+Error in `[.data.frame`(dat, , c("s", "l")) : undefined columns selected
