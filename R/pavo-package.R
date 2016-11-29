060cf17599cfaae6f444b2bf92f32b1969ee5b87
rmaia <rm72@zips.uakron.edu>
2016-11-29 17:45:46 -0500

    trying to preemptively remove CRAN notes

--- R/pavo-package.R
+++ R/pavo-package.R
@@ -19,8 +19,10 @@
 #' @keywords package
 #' @examples #see functions.
 #' @importFrom grDevices col2rgb colorRampPalette rgb
-#' @importFrom graphics abline axis image legend lines mtext par plot points polygon title
+#' @importFrom graphics abline axis image legend lines mtext par plot points polygon title arrows
 #' @importFrom stats approx cor dist loess.smooth median quantile runif sd var
 #' @importFrom utils combn head read.table setTxtProgressBar tail txtProgressBar
+#' @importFrom rgl open3d spheres3d
+#' @importFrom base <<-
 
 NULL
