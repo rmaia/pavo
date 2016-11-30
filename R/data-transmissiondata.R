b502497029eb371cc3763f1172dad0a8c3e07f7f
rmaia <rm72@zips.uakron.edu>
2016-11-30 15:06:40 -0500

    making transmission great again

---
+++ R/data-transmissiondata.R
@@ -0,0 +1,11 @@
+#' Default ocular transmission data
+#'
+#' @name transmissiondata
+#' @docType data
+#' @author Rafael Maia \email{rm72@@zips.uakron.edu}
+#' @references Hart, N. S., Partridge, J. C., Cuthill, I. C., Bennett, A. T. D. (2000). 
+#' Visual pigments, oil droplets, ocular media and cone photoreceptor distribution in two
+#' species of passerine 
+#' @keywords data
+#' @export transmissiondata
+NULL
