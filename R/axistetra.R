fa5189728a74fb82edcc884a09bc222793a03754
rmaia <rm72@zips.uakron.edu>
2016-12-01 10:09:44 -0500

    axistetra: changed adj.label argument name

--- R/axistetra.R
+++ R/axistetra.R
@@ -9,7 +9,7 @@
 #' @param arrowhead size of the arrowhead.
 #' @param col,lty,lwd graphical parameters for the arrows.
 #' @param label logical, include x, y and z labels (defaults to TRUE).
-#' @param label.adj position adjustment for the labels. Can be either a 
+#' @param adj.label position adjustment for the labels. Can be either a 
 #' single value (applied for x, y and z) or a vector of 3 separate values
 #' for each axis.
 #' @param label.cex,label.col graphical parameters for the labels.
@@ -24,7 +24,7 @@
 axistetra <- function(x = 0.6, y = 0.8, z = 0.7, size = 0.1, 
                       arrowhead = 0.05, col = par("fg"), 
                       lty = par("lty"), lwd = par("lwd"),
-                      label = TRUE, label.adj = 0.03, label.cex = 1, label.col = NULL){
+                      label = TRUE, adj.label = 0.03, label.cex = 1, label.col = NULL){
   
   if(length(size) > 1){
   	lx <- size[1]
@@ -49,24 +49,18 @@ axistetra <- function(x = 0.6, y = 0.8, z = 0.7, size = 0.1,
   
   if(label){
 
-  if(length(label.adj) > 1){
-  	px <- 1+label.adj[1]
-  	py <- 1+label.adj[2]/2
-  	pz <- 1+label.adj[3]
+  if(length(adj.label) > 1){
+  	px <- 1 + adj.label[1]
+  	py <- 1 + adj.label[2]/2
+  	pz <- 1 + adj.label[3]
   } else{
-  	px <- pz <- 1+label.adj[1]
-  	py <- 1 + label.adj[1]/2
+  	px <- pz <- 1+ adj.label[1]
+  	py <- 1 + adj.label[1]/2
   }
  	
   	text(xarr$x[2]*px, xarr$y[2], labels='x', cex=label.cex, col=label.col)
   	text(yarr$x[2]*py, yarr$y[2]*py, labels='y', cex=label.cex, col=label.col)
   	text(zarr$x[2], zarr$y[2]*pz, labels='z', cex=label.cex, col=label.col)
-  	
-#pj <- 1 + label.adj  	
-#text(xarr$x[2]*pj,xarr$y[2], 'x')
-#text(yarr$x[2]*pj,yarr$y[2]*pj, 'y')
-#text(zarr$x[2],zarr$y[2]*pj, 'z')
-
   }
 }
 
