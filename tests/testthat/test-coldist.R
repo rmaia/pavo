ee457aaf142b1a1dca6137b0b99203723c017dd0
thomased <thomas.white026@gmail.com>
2017-05-30 09:36:12 +1000

    Add tests

--- tests/testthat/test-coldist.R
+++ tests/testthat/test-coldist.R
@@ -4,32 +4,32 @@ context('coldist')
 test_that('Errors', {
   data(flowers)
   
-  expect_error(coldist(vismodel(flowers), v=0.2), "deprecated")
-  expect_error(coldist(vismodel(flowers), n1=1), "deprecated")
-  expect_error(coldist(vismodel(flowers), n2=2), "deprecated")
-  expect_error(coldist(vismodel(flowers), n3=2), "deprecated")
-  expect_error(coldist(vismodel(flowers), n4=4), "deprecated")
+  expect_error(coldist(vismodel(flowers), v = 0.2), "deprecated")
+  expect_error(coldist(vismodel(flowers), n1 = 1), "deprecated")
+  expect_error(coldist(vismodel(flowers), n2 = 2), "deprecated")
+  expect_error(coldist(vismodel(flowers), n3 = 2), "deprecated")
+  expect_error(coldist(vismodel(flowers), n4 = 4), "deprecated")
   
-  expect_error(coldist(vismodel(flowers, relative=FALSE), n=c(1,2,3,4,5)), "different length")
-  expect_error(coldist(vismodel(flowers, relative=FALSE), n=c(1,2,3)), "different length")
+  expect_error(coldist(vismodel(flowers, relative = FALSE), n = c(1, 2, 3, 4, 5), achro = FALSE), "different length")
+  expect_error(coldist(vismodel(flowers, relative = FALSE), n = c(1, 2, 3), achro = FALSE), "different length")
 })
 
 test_that('Equivalent', {
   data(flowers)
   
   expect_equal(
-    coldist(vismodel(flowers, relative=FALSE), weber.ref='longest'), 
-	coldist(vismodel(flowers, relative=FALSE), weber.ref=4)
+    coldist(vismodel(flowers, relative = FALSE), weber.ref = 'longest', achro = FALSE), 
+	  coldist(vismodel(flowers, relative = FALSE), weber.ref = 4, achro = FALSE)
 	)
 	
   expect_equal(
-    coldist(vismodel(flowers, relative=FALSE), weber.ref='longest'), 
-    coldist(vismodel(flowers, relative=FALSE), weber.ref=4)
+    coldist(vismodel(flowers, relative = FALSE), weber.ref = 'longest', achro = FALSE), 
+    coldist(vismodel(flowers, relative = FALSE), weber.ref = 4, achro = FALSE)
     )
 
   expect_equal(
-    coldist(vismodel(flowers, relative=FALSE), achro=FALSE), 
-    suppressWarnings(coldist(colspace(vismodel(flowers, relative=FALSE)), achro=FALSE)),
+    coldist(vismodel(flowers, relative = FALSE), achro = FALSE), 
+    suppressWarnings(coldist(colspace(vismodel(flowers, relative = FALSE)), achro = FALSE)),
     check.attributes = FALSE)	
 
 })
