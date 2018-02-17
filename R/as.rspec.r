8b38bc2e0a682f4483548e83af0381c43cce6b96
Hugo Gruson <hugo.gruson@protonmail.com>
2018-02-17 13:22:34 +0100

    Remove unnecessary paste

--- R/as.rspec.r
+++ R/as.rspec.r
@@ -65,7 +65,7 @@ as.rspec <- function(object, whichwl = NULL, interp = TRUE, lim = NULL) {
     wl <- object[, wl_index]
     object <- as.data.frame(object[, -wl_index])
     name <- name[-wl_index]
-    message(paste0("wavelengths found in column ", wl_index))
+    message("wavelengths found in column ", wl_index)
   } else if (!is.null(lim)) {
     wl <- seq(lim[1], lim[2], length = nrow(object))
     object <- as.data.frame(object)
