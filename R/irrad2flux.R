1e3f08b66ba06899982f0d9f2d1b883da724018b
Hugo Gruson <hugo.gruson@protonmail.com>
2018-05-06 09:03:39 +0200

    Rewrap doc to 80 characters

--- R/irrad2flux.R
+++ R/irrad2flux.R
@@ -1,9 +1,10 @@
 #' Converts between irradiance and photon (quantum) flux
 #'
-#' Some spectrometers will give illuminant values in units of irradiance (uWatt * cm^-2),
-#' but physiological models require illuminants in units of photon (quantum) flux 
-#' (umol * s^-1 * m^-2). The functions \code{irrad2flux} and \code{flux2irrad} allows for
-#' easy conversion of \code{rspec} objects between these units.
+#' Some spectrometers will give illuminant values in units of irradiance 
+#' (uWatt * cm^-2), but physiological models require illuminants in units of 
+#' photon (quantum) flux (umol * s^-1 * m^-2). The functions \code{irrad2flux} 
+#' and \code{flux2irrad} allows for easy conversion of \code{rspec} objects
+#' between these units.
 #'
 #' @param rspecdata (required) a rspec object containing illuminant values.
 #' @return a converted \code{rspec} object.
@@ -28,7 +29,6 @@ irrad2flux <- function(rspecdata){
 
 
 #' @rdname irrad2flux
-#' @return a converted \code{rspec} object
 
 flux2irrad <- function(rspecdata){
 
