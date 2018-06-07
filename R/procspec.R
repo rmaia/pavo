5bd4c04bf54966d3f1699fc630ad7993086b6971
Hugo Gruson <hugo.gruson@protonmail.com>
2018-06-07 21:48:33 +0200

    Fix bin width formula

--- R/procspec.R
+++ R/procspec.R
@@ -9,7 +9,7 @@
 #'            by providing a vector. Possibilities are:
 #' \itemize{
 #'	\item \code{"none"} does not perform any processing (default).
-#'	\item \code{"smooth"} applies LOESS smoothing to each spectrum using 
+#'	\item \code{"smooth"} applies LOESS smoothing to each spectrum using
 #'                      \code{\link{loess.smooth}}. Optimal smoothing parameter
 #'                      can be assessed by using \code{\link{plotsmooth}}.
 #' 	\item \code{"minimum"} subtracts the minimum from each individual spectra.
@@ -17,12 +17,12 @@
 #' 	\item \code{"sum"} divides each spectrum by summed values.
 #' 	\item \code{"bin"} bins each spectrum into specified wavelength ranges. User should
 #'									 specify.
-#'  \item \code{"center"} centers individual spectra by subtracting mean reflectance from 
+#'  \item \code{"center"} centers individual spectra by subtracting mean reflectance from
 #'                      all values.
 #' }
 #'
 #' @param fixneg how to handle negative values. Possibilities are:
-#' \itemize{ 
+#' \itemize{
 #'	\item \code{"none"} does not perform negative value correction (default).
 #'    \item \code{"zero"} sets all negative values to zero.
 #'    \item \code{"addmin"} adds the absolute value of the maximally negative values of each
@@ -32,37 +32,37 @@
 #' @param span sets the smoothing parameter used by \code{loess.smooth}.
 #' @param bins sets the number of equally sized wavelength bins for \code{opt = "bin"}.
 #' @param ... ignored.
-#' 
+#'
 #' @return A data frame of class \code{rspec} with the processed data.
-#' 
+#'
 #' @export
-#' 
+#'
 #' @author Chad Eliason \email{cme16@@zips.uakron.edu}
-#' 
+#'
 #' @examples \dontrun{
 #' data(teal)
 #' plot(teal, select = 10)
-#' 
+#'
 #' # Smooth data to remove noise
 #' teal.sm <- procspec(teal, opt = 'smooth', span = 0.25)
 #' plot(teal.sm, select = 10)
-#' 
+#'
 #' # Normalize to max of unity
 #' teal.max <- procspec(teal, opt = c('max'), span = 0.25)
 #' plot(teal.max, select = 10)
 #' }
-#' 
+#'
 #' @seealso \code{\link{loess.smooth}}
-#' 
-#' @references Cuthill, I., Bennett, A. T. D., Partridge, J. & Maier, E. 1999. 
-#'  Plumage reflectance and the objective assessment of avian sexual dichromatism. 
+#'
+#' @references Cuthill, I., Bennett, A. T. D., Partridge, J. & Maier, E. 1999.
+#'  Plumage reflectance and the objective assessment of avian sexual dichromatism.
 #'  The American Naturalist, 153, 183-200.
-#' @references Montgomerie R. 2006. Analyzing colors. In Hill, G.E, and McGraw, 
-#'  K.J., eds. Bird Coloration. Volume 1 Mechanisms and measurements. Harvard 
+#' @references Montgomerie R. 2006. Analyzing colors. In Hill, G.E, and McGraw,
+#'  K.J., eds. Bird Coloration. Volume 1 Mechanisms and measurements. Harvard
 #'  University Press, Cambridge, Massachusetts.
 
-procspec <- function(rspecdata, opt = c('none', 'smooth', 'maximum', 'minimum', 
-										 'bin', 'sum', 'center'), 
+procspec <- function(rspecdata, opt = c('none', 'smooth', 'maximum', 'minimum',
+										 'bin', 'sum', 'center'),
 										 fixneg = c('none', 'addmin', 'zero'),
 										 span = 0.25, bins = 20, ...) {
 
@@ -74,7 +74,7 @@ applied <- 'processing options applied:\n'
 
 if (any(opt=='none')) {
   opt <- 'none' # remove other opt arguments (so they are not called further on, but still allowing for fixneg to work)
-  
+
   if(fixneg=='none')
     stop('No processing options selected')
   }
@@ -93,19 +93,19 @@ if (length(wl_index > 0)){
 nam <- names(rspecdata)
 
 if (any(opt=='smooth')){
-  rspecdata <- sapply(1:ncol(rspecdata), function(z){loess.smooth(x = wl, 
-                  y = as.data.frame(rspecdata[, z]), span = span, degree = 2, 
+  rspecdata <- sapply(1:ncol(rspecdata), function(z){loess.smooth(x = wl,
+                  y = as.data.frame(rspecdata[, z]), span = span, degree = 2,
                   family = "gaussian", evaluation = length(wl))$y})
   applied <- c(applied, paste('smoothing spectra with a span of',span,'\n'))
   }
 
 # if (any(opt=='smooth')&method=='spline')
-  # rspecdata <- sapply(names(rspecdata), function(z){smooth.spline(x = wl, y = rspecdata[, z], 
+  # rspecdata <- sapply(names(rspecdata), function(z){smooth.spline(x = wl, y = rspecdata[, z],
            # spar = spar)$y})
 
 # if (any(opt=='smooth')&method=='loess')
-  # rspecdata <- sapply(names(rspecdata), function(z){loess.smooth(x = wl, y = rspecdata[, z], 
-           # span = span, degree = 2, family = "gaussian", 
+  # rspecdata <- sapply(names(rspecdata), function(z){loess.smooth(x = wl, y = rspecdata[, z],
+           # span = span, degree = 2, family = "gaussian",
            # evaluation = length(wl))$y})
 
 if (fixneg=='addmin'){
@@ -150,15 +150,15 @@ if (any(opt=='center')){
 # Calculate medians according to # of bins specified for use in PCA
 # Method follows Cuthill et al. (1999)
 if (any(opt=='bin')) {
-  bw <- floor(length(wl)/bins)
-  wl_bin <- seq(head(wl,1), tail(wl,1), by=bw)
+  bw <- floor(length(wl)/(bins-1))
+  wl_bin <- seq(min(wl), by=bw, lengh.out = bins)
   wl_ind <- match(wl_bin, wl)
-  rspecdata <- sapply(1:(length(wl_ind)-1), function(z) 
-                      apply(rspecdata[wl_ind[z]:(wl_ind[z] + bw),,drop=F], 2, median),
+  rspecdata <- sapply(1:length(wl_ind), function(z)
+                      apply(rspecdata[wl_ind[z]:(wl_ind[z] + bw),,drop=F], 2, median, na.rm = TRUE),
                       simplify=FALSE)
 
   rspecdata <- data.frame(matrix(unlist(rspecdata), nrow=bins, byrow=T))
-  rspecdata <- as.data.frame(cbind(wl_bin[-length(wl_bin)], rspecdata))
+  rspecdata <- as.data.frame(cbind(wl_bin, rspecdata))
   applied <- c(applied, paste('binned spectra to ',bw,'-nm intervals\n',sep=''))
   } else {
     rspecdata <- as.data.frame(cbind(wl, rspecdata))
