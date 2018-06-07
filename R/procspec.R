bcb95e9ef84078725ed9c6623016a5caa922da0f
Hugo Gruson <hugo.gruson@protonmail.com>
2018-06-07 21:48:47 +0200

    Run styler

--- R/procspec.R
+++ R/procspec.R
@@ -8,26 +8,26 @@
 #' @param opt what type of processing options to apply. User can select multiple options
 #'            by providing a vector. Possibilities are:
 #' \itemize{
-#'	\item \code{"none"} does not perform any processing (default).
-#'	\item \code{"smooth"} applies LOESS smoothing to each spectrum using
+#' 	\item \code{"none"} does not perform any processing (default).
+#' 	\item \code{"smooth"} applies LOESS smoothing to each spectrum using
 #'                      \code{\link{loess.smooth}}. Optimal smoothing parameter
 #'                      can be assessed by using \code{\link{plotsmooth}}.
 #' 	\item \code{"minimum"} subtracts the minimum from each individual spectra.
 #' 	\item \code{"maxmimum"} divides each spectrum by its maximum value.
 #' 	\item \code{"sum"} divides each spectrum by summed values.
 #' 	\item \code{"bin"} bins each spectrum into specified wavelength ranges. User should
-#'									 specify.
+#' 									 specify.
 #'  \item \code{"center"} centers individual spectra by subtracting mean reflectance from
 #'                      all values.
 #' }
 #'
 #' @param fixneg how to handle negative values. Possibilities are:
 #' \itemize{
-#'	\item \code{"none"} does not perform negative value correction (default).
-#'    \item \code{"zero"} sets all negative values to zero.
-#'    \item \code{"addmin"} adds the absolute value of the maximally negative values of each
-#'                           spectra to the reflectance at all other wavelengths (setting
-#'                           the minimum value to zero, but scaling other values accordingly).
+#' 	\item \code{"none"} does not perform negative value correction (default).
+#'  \item \code{"zero"} sets all negative values to zero.
+#'  \item \code{"addmin"} adds the absolute value of the maximally negative values of each
+#'                      spectra to the reflectance at all other wavelengths (setting
+#'                      the minimum value to zero, but scaling other values accordingly).
 #' }
 #' @param span sets the smoothing parameter used by \code{loess.smooth}.
 #' @param bins sets the number of equally sized wavelength bins for \code{opt = "bin"}.
@@ -61,115 +61,122 @@
 #'  K.J., eds. Bird Coloration. Volume 1 Mechanisms and measurements. Harvard
 #'  University Press, Cambridge, Massachusetts.
 
-procspec <- function(rspecdata, opt = c('none', 'smooth', 'maximum', 'minimum',
-										 'bin', 'sum', 'center'),
-										 fixneg = c('none', 'addmin', 'zero'),
-										 span = 0.25, bins = 20, ...) {
+procspec <- function(rspecdata, opt = c(
+                       "none", "smooth", "maximum", "minimum",
+                       "bin", "sum", "center"
+                     ),
+                     fixneg = c("none", "addmin", "zero"),
+                     span = 0.25, bins = 20, ...) {
+  opt <- match.arg(opt, several.ok = TRUE)
 
-opt <- match.arg(opt, several.ok = TRUE)
+  fixneg <- match.arg(fixneg)
 
-fixneg <- match.arg(fixneg)
+  applied <- "processing options applied:\n"
 
-applied <- 'processing options applied:\n'
+  if (any(opt == "none")) {
+    opt <- "none" # remove other opt arguments (so they are not called further on, but still allowing for fixneg to work)
 
-if (any(opt=='none')) {
-  opt <- 'none' # remove other opt arguments (so they are not called further on, but still allowing for fixneg to work)
-
-  if(fixneg=='none')
-    stop('No processing options selected')
+    if (fixneg == "none") {
+      stop("No processing options selected")
+    }
   }
 
-wl_index <- which(names(rspecdata)=='wl')
+  wl_index <- which(names(rspecdata) == "wl")
 
-if (length(wl_index > 0)){
-  wl <- rspecdata[, wl_index]
-  rspecdata <- as.data.frame(rspecdata[-wl_index])
-    } else {
-    warning('No wavelengths supplied; using arbitrary values')
+  if (length(wl_index > 0)) {
+    wl <- rspecdata[, wl_index]
+    rspecdata <- as.data.frame(rspecdata[-wl_index])
+  } else {
+    warning("No wavelengths supplied; using arbitrary values")
     rspecdata <- as.data.frame(rspecdata)
     wl <- 1:nrow(rspecdata)
-    }
-
-nam <- names(rspecdata)
-
-if (any(opt=='smooth')){
-  rspecdata <- sapply(1:ncol(rspecdata), function(z){loess.smooth(x = wl,
-                  y = as.data.frame(rspecdata[, z]), span = span, degree = 2,
-                  family = "gaussian", evaluation = length(wl))$y})
-  applied <- c(applied, paste('smoothing spectra with a span of',span,'\n'))
   }
 
-# if (any(opt=='smooth')&method=='spline')
+  nam <- names(rspecdata)
+
+  if (any(opt == "smooth")) {
+    rspecdata <- sapply(1:ncol(rspecdata), function(z) {
+      loess.smooth(
+        x = wl,
+        y = as.data.frame(rspecdata[, z]), span = span, degree = 2,
+        family = "gaussian", evaluation = length(wl)
+      )$y
+    })
+    applied <- c(applied, paste("smoothing spectra with a span of", span, "\n"))
+  }
+
+  # if (any(opt=='smooth')&method=='spline')
   # rspecdata <- sapply(names(rspecdata), function(z){smooth.spline(x = wl, y = rspecdata[, z],
-           # spar = spar)$y})
+  # spar = spar)$y})
 
-# if (any(opt=='smooth')&method=='loess')
+  # if (any(opt=='smooth')&method=='loess')
   # rspecdata <- sapply(names(rspecdata), function(z){loess.smooth(x = wl, y = rspecdata[, z],
-           # span = span, degree = 2, family = "gaussian",
-           # evaluation = length(wl))$y})
+  # span = span, degree = 2, family = "gaussian",
+  # evaluation = length(wl))$y})
 
-if (fixneg=='addmin'){
-  adm = function(x){
-    if (min(x) < 0){ x + abs(min(x))
-    }else{
-    x
+  if (fixneg == "addmin") {
+    adm <- function(x) {
+      if (min(x) < 0) {
+        x + abs(min(x))
+      } else {
+        x
+      }
     }
-  }
-  tempspc <- data.frame(sapply(1:ncol(rspecdata), function(z) adm(rspecdata[, z]) ) )
-  names(tempspc) <- names(rspecdata)
-  rspecdata <- round(tempspc,6)
-  applied <- c(applied, 'Negative value correction: added min to all reflectance\n')
-}
-
-if (fixneg=='zero'){
-  rspecdata[rspecdata < 0 ] <- 0
-  applied <- c(applied, 'Negative value correction: converted negative values to zero\n')
-}
-
-
-if (any(opt=='minimum')){
-  rspecdata <- sapply(1:ncol(rspecdata), function(z)rspecdata[, z] - min(rspecdata[, z], ...))
-   applied <- c(applied, 'Scaling spectra to a minimum value of zero\n')
+    tempspc <- data.frame(sapply(1:ncol(rspecdata), function(z) adm(rspecdata[, z])))
+    names(tempspc) <- names(rspecdata)
+    rspecdata <- round(tempspc, 6)
+    applied <- c(applied, "Negative value correction: added min to all reflectance\n")
   }
 
-if (any(opt=='maximum')){
-  rspecdata <- sapply(1:ncol(rspecdata), function(z)rspecdata[, z] / max(rspecdata[, z], ...))
-   applied <- c(applied, 'Scaling spectra to a maximum value of 1\n')
+  if (fixneg == "zero") {
+    rspecdata[rspecdata < 0 ] <- 0
+    applied <- c(applied, "Negative value correction: converted negative values to zero\n")
   }
 
-if (any(opt=='sum')){
-  rspecdata <- sapply(1:ncol(rspecdata), function(z)rspecdata[, z] / sum(rspecdata[, z], ...))
-   applied <- c(applied, 'Scaling spectra to a total area of 1\n')
+
+  if (any(opt == "minimum")) {
+    rspecdata <- sapply(1:ncol(rspecdata), function(z) rspecdata[, z] - min(rspecdata[, z], ...))
+    applied <- c(applied, "Scaling spectra to a minimum value of zero\n")
   }
 
-if (any(opt=='center')){
-  rspecdata <- sapply(1:ncol(rspecdata), function(z)rspecdata[, z] - mean(rspecdata[, z], ...))
-   applied <- c(applied, 'Centering spectra to a mean of zero\n')
+  if (any(opt == "maximum")) {
+    rspecdata <- sapply(1:ncol(rspecdata), function(z) rspecdata[, z] / max(rspecdata[, z], ...))
+    applied <- c(applied, "Scaling spectra to a maximum value of 1\n")
   }
 
-# Calculate medians according to # of bins specified for use in PCA
-# Method follows Cuthill et al. (1999)
-if (any(opt=='bin')) {
-  bw <- floor(length(wl)/(bins-1))
-  wl_bin <- seq(min(wl), by=bw, lengh.out = bins)
-  wl_ind <- match(wl_bin, wl)
-  rspecdata <- sapply(1:length(wl_ind), function(z)
-                      apply(rspecdata[wl_ind[z]:(wl_ind[z] + bw),,drop=F], 2, median, na.rm = TRUE),
-                      simplify=FALSE)
+  if (any(opt == "sum")) {
+    rspecdata <- sapply(1:ncol(rspecdata), function(z) rspecdata[, z] / sum(rspecdata[, z], ...))
+    applied <- c(applied, "Scaling spectra to a total area of 1\n")
+  }
 
-  rspecdata <- data.frame(matrix(unlist(rspecdata), nrow=bins, byrow=T))
-  rspecdata <- as.data.frame(cbind(wl_bin, rspecdata))
-  applied <- c(applied, paste('binned spectra to ',bw,'-nm intervals\n',sep=''))
+  if (any(opt == "center")) {
+    rspecdata <- sapply(1:ncol(rspecdata), function(z) rspecdata[, z] - mean(rspecdata[, z], ...))
+    applied <- c(applied, "Centering spectra to a mean of zero\n")
+  }
+
+  # Calculate medians according to # of bins specified for use in PCA
+  # Method follows Cuthill et al. (1999)
+  if (any(opt == "bin")) {
+    bw <- floor(length(wl) / (bins - 1))
+    wl_bin <- seq(min(wl), by = bw, length.out = bins)
+    wl_ind <- match(wl_bin, wl)
+    rspecdata <- sapply(1:length(wl_ind), function(z)
+      apply(rspecdata[wl_ind[z]:(wl_ind[z] + bw), , drop = F], 2, median, na.rm = TRUE),
+    simplify = FALSE
+    )
+
+    rspecdata <- data.frame(matrix(unlist(rspecdata), nrow = bins, byrow = T))
+    rspecdata <- as.data.frame(cbind(wl_bin, rspecdata))
+    applied <- c(applied, paste("binned spectra to ", bw, "-nm intervals\n", sep = ""))
   } else {
     rspecdata <- as.data.frame(cbind(wl, rspecdata))
-    }
+  }
 
-names(rspecdata) <- c('wl', nam)
-class(rspecdata) <- c('rspec', 'data.frame')
+  names(rspecdata) <- c("wl", nam)
+  class(rspecdata) <- c("rspec", "data.frame")
 
-applied <- c(applied, '\n')
-cat(applied)
+  applied <- c(applied, "\n")
+  cat(applied)
 
-rspecdata
-
+  rspecdata
 }
