b73f585b2a60e105e85be44ccedf9c9fb3376955
thomased <thomas.white026@gmail.com>
2018-07-26 19:46:31 +1000

    refactoring

--- R/plot.rimg.R
+++ R/plot.rimg.R
@@ -33,22 +33,11 @@ plot.rimg <- function(x, axes = TRUE, col = NULL, ...) {
   multi_image <- inherits(x, "list") # Single or multiple images?
 
   if (!multi_image) {
-    if (attr(x, "state") == "raw") {
-      defaultrasterImageplot(x, axes = axes, ...)
-    } else if (attr(x, "state") == "colclass") {
-      defaultrasterImageplot(x, axes = axes, col = col, ...)
-    }
+    defaultrasterImageplot(x, axes = axes, col = col, ...)
   } else if (multi_image) {
-    if (attr(x[[1]], "state") == "raw") {
-      for (i in 1:length(x)) {
-        readline(prompt = "Press [enter] for next plot")
-        defaultrasterImageplot(x[[i]], axes = axes, ...)
-      }
-    } else if (attr(x[[1]], "state") == "colclass") {
-      for (i in 1:length(x)) {
-        readline(prompt = "Press [enter] for next plot")
-        defaultrasterImageplot(x[[i]], axes = axes, col = col, ...)
-      }
+    for (i in 1:length(x)) {
+      readline(prompt = "Press [enter] for next plot")
+      defaultrasterImageplot(x[[i]], axes = axes, col = col, ...)
     }
   }
 }
@@ -69,23 +58,23 @@ defaultrasterImageplot <- function(imagedata, axes, col, ...) {
 
     # Reconstitute image
     if (!is.null(col)) {
-      rgbs <- as.data.frame(t(col2rgb(col)/255))
-      names(rgbs) <- c('R', 'G', 'B')
+      rgbs <- as.data.frame(t(col2rgb(col) / 255))
+      names(rgbs) <- c("R", "G", "B")
     } else {
       rgbs <- attr(imagedata, "classRGB")
     }
-    R <- G <- B <- img
-    for (i in 1:nrow(rgbs)) R[which(R == row.names(rgbs)[i])] <- rgbs$R[i]
-    for (i in 1:nrow(rgbs)) G[which(G == row.names(rgbs)[i])] <- rgbs$G[i]
-    for (i in 1:nrow(rgbs)) B[which(B == row.names(rgbs)[i])] <- rgbs$B[i]
 
-    imageout <- array(dim = c(dim(img)[1], dim(img)[2], 3))
-    imageout[, , 1] <- R
-    imageout[, , 2] <- G
-    imageout[, , 3] <- B
+    mapR <- setNames(rgbs$R, 1:nrow(rgbs))
+    mapG <- setNames(rgbs$G, 1:nrow(rgbs))
+    mapB <- setNames(rgbs$B, 1:nrow(rgbs))
+    R <- matrix(mapR[img], nrow = nrow(img), dimnames = dimnames(img))
+    G <- matrix(mapG[img], nrow = nrow(img), dimnames = dimnames(img))
+    B <- matrix(mapB[img], nrow = nrow(img), dimnames = dimnames(img))
+
+    imageout <- array(c(R, G, B), dim = c(dim(img)[1], dim(img)[2], 3))
 
     imagedata2 <- suppressWarnings(as.raster(as.cimg(imageout, cc = 3)))
-  } else {
+  } else if (attr(imagedata, "state") == "raw") {
     imagedata2 <- suppressWarnings(as.raster(as.cimg(imagedata, cc = 3)))
   }
 
@@ -110,30 +99,3 @@ defaultrasterImageplot <- function(imagedata, axes, col, ...) {
   title(arg$main, xlab = arg$xlab, ylab = arg$ylab)
   rasterImage(imagedata2, 1, nrow(imagedata2), ncol(imagedata2), 1)
 }
-
-## For colour-classified images
-# defaultimageplot <- function(rawimage, axes, col = NULL, ...) {
-# 
-#   # Transform to present the damn correct orientation
-#   imagedata <- as.matrix(t(apply(rawimage, 2, rev)))
-# 
-#   # Defaults
-#   arg <- list(...)
-#   if (is.null(arg$xlab)) arg$xlab <- "x"
-#   if (is.null(arg$ylab)) arg$ylab <- "y"
-#   if (is.null(arg$main)) arg$main <- attr(rawimage, "imgname")
-#   if (is.null(arg$asp)) arg$asp <- ncol(imagedata) / nrow(imagedata)
-#   if (is.null(arg$xlim)) arg$xlim <- c(1, nrow(imagedata))
-#   if (is.null(arg$ylim)) arg$ylim <- c(ncol(imagedata), 1)
-#   if (is.null(col)) col <- rgb(attr(rawimage, "classRGB"))
-# 
-#   plot.new()
-#   do.call(plot.window, arg)
-#   if (axes) {
-#     axis(1)
-#     axis(2)
-#     box()
-#   }
-#   title(arg$main, xlab = arg$xlab, ylab = arg$ylab)
-#   image(1:nrow(imagedata), 1:ncol(imagedata), imagedata, add = TRUE, useRaster = TRUE, col = col)
-# }
