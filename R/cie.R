ae4224235d8197d656266ef53bcd71df9099d08e
Hugo Gruson <hugo.gruson@protonmail.com>
2018-11-30 11:42:31 +0100

    Fix typos

--- R/cie.R
+++ R/cie.R
@@ -1,12 +1,12 @@
 #' CIE colour spaces
 #'
 #' Calculates coordinates and colorimetric variables that represent reflectance spectra
-#' in either the CIEXYZ (1931), CIELAB (1971), or CIELCH (1971) colourspaces.
+#' in either the CIEXYZ (1931), CIELAB (1971), or CIELCh (1971) colourspaces.
 #'
 #' @param vismodeldata (required) quantum catch color data. Can be either the result
 #'  from \code{\link{vismodel}} or independently calculated data (in the form of a
 #'  data frame with three columns representing trichromatic viewer).
-#' @param space (required) Choice between XYZ (1931), LAB (1971), or LCH colour models.
+#' @param space (required) Choice between XYZ (1931), LAB (1971), or LCh colour models.
 #'
 #' @return Object of class \code{colspace} containing:
 #'    \itemize{
