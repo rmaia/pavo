f2565f6eb046e965dc100f9c2f24f296f9821e8b
Thomas White <thomas.white026@gmail.com>
2018-12-02 10:02:11 +1100

    force vignette rebuild

--- inst/doc/overview.R
+++
@@ -1,8 +0,0 @@
-## ----include = FALSE-----------------------------------------------------
-# Do not use partial matching
-options(
-   warnPartialMatchDollar = FALSE,
-   warnPartialMatchArgs = FALSE,
-   warnPartialMatchAttr = FALSE
-)
-
