f1f48a9546b535a0da8580c3e510b091e11e14ae
Thomas White <thomas.white026@gmail.com>
2019-01-16 21:36:44 +1100

    Add titles to avoid warning when building docs

--- R/is.colspace.R
+++ R/is.colspace.R
@@ -1,3 +1,7 @@
+#' Test is object is of class 'colspace
+#' 
+#' @param object an R object
+#' 
 #' @return a logical value indicating whether the object is of class \code{colspace}
 is.colspace <- function(object) {
   inherits(object, "colspace")
--- R/is.vismodel.R
+++ R/is.vismodel.R
@@ -1,3 +1,7 @@
+#' Test is object is of class 'vismodel'
+#' 
+#' @param object an R object
+#'  
 #' @return a logical value indicating whether the object is of class \code{vismodel}
 is.vismodel <- function(object) {
   inherits(object, "vismodel")
