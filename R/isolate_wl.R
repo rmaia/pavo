668520968e0a546854ae08c1724f738cd2ab87a9
Hugo Gruson <hugo.gruson@protonmail.com>
2019-07-31 14:44:39 +0200

    Use better default range

--- R/isolate_wl.R
+++ R/isolate_wl.R
@@ -1,14 +1,16 @@
 isolate_wl <- function(rspecdata, keep = c("wl", "spec")) {
 
+  keep <- match.arg(keep)
+
   is_wl <- colnames(rspecdata) == "wl"
 
   if (keep == "wl") {
     if (any(is_wl)) {
       return(rspecdata[, is_wl])
     } else {
-      warning("wl column missing from input rspec data. Defaulting to 300-700",
-              "nm range.", call. = FALSE)
-      return(300:700)
+      warning("wl column missing from input rspec data. Using arbritrary ",
+              "values based on object length.", call. = FALSE)
+      return(seq_len(nrow(rspecdata)))
     }
   } else {
     return(rspecdata[, !is_wl, drop = FALSE])
