dcc75643626f502a3fdb68ac9f2058beb37bec21
Hugo Gruson <hugo.gruson@protonmail.com>
2019-11-26 18:10:21 +0100

    Put alphashape functions in pavo

---
+++ R/tcsshape.R
@@ -0,0 +1,55 @@
+#' Produces a 3D alphashape in tetrahedral colour space
+#'
+#' @inheritParams tcsvol
+#' @inheritParams tetrashape
+#'
+#' @export
+#'
+#' @examples
+#' # For plotting
+#' data(sicalis)
+#' vis.sicalis <- vismodel(sicalis, visual = "avg.uv")
+#' tcs.sicalis <- colspace(vis.sicalis, space = "tcs")
+#' tcsplot(tcs.sicalis, col = "blue", size = 0.005)
+#' tcsshape(tcs.sicalis, avalue = 0.1)
+#'
+#' @importFrom alphashape3d ashape3d
+
+tcsshape <- function (tcsdata, col = "black", alpha = 0.2, grid.alpha = 1,
+                      grid = TRUE, fill = TRUE, lwd = 1, avalue) {
+  if (attr(tcsdata, "clrsp") != "tcs") stop("object is not in tetrahedral color space")
+
+  # check if rgl is installed and loaded
+  if (!requireNamespace("rgl", quietly = TRUE)) {
+    stop(dQuote("rgl"), " package needed for this function to work. Please install it.",
+         call. = FALSE
+    )
+  }
+
+  if (!isNamespaceLoaded("rgl")) {
+    requireNamespace("rgl")
+  }
+
+  ashape <- ashape3d(as.matrix(tcsdata[, c("x", "y", "z")]), alpha = avalue)
+  tri <- ashape$triang
+  vol <- t(tri[tri[, ncol(tri)] %in% c(2,3), c(1, 2, 3)])
+  coords <- ashape$x
+  listvol <- split(vol, rep(seq_len(ncol(vol)), each = nrow(vol)))
+  ppairs <- do.call(rbind, lapply(listvol, function(x) t(combn(x,
+                                                               2))))
+
+  if (grid) {
+    rgl::triangles3d(coords[vol, ],
+                     color = col, alpha = grid.alpha, lwd = lwd,
+                     front = "lines", back = "lines"
+    )
+  }
+
+  if (fill) {
+    rgl::rgl.triangles(coords[vol, ],
+                       alpha = alpha, color = col
+    )
+  }
+
+  rgl::material3d(alpha = 1)
+}
