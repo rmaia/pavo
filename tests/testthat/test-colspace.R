rmaia/pavo

commit 1a9a8dbbb30fdaec378c5c2ef35d007f64bdbcf6

Split the space auto-assignment off from the model calls

R/colspace.R
@@ -99,9 +99,11 @@ colspace <- function(vismodeldata,
     stop("Invalid colorspace selected")
   }
 
+  # Auto-define an appropriate space
   if (space2 == "auto") {
     if (all(c("X", "Y", "Z") %in% names(vismodeldata))) {
-      res <- cie(vismodeldata, "XYZ")
+      space2 <- 'XYZ'
+      #res <- cie(vismodeldata, "XYZ")
     } else {
       if (is.null(attr(vismodeldata, "conenumb"))) {
         stop(
@@ -110,15 +112,17 @@ colspace <- function(vismodeldata,
           " argument"
         )
       }
-      res <-
+      space2 <-
         switch(as.character(attr(vismodeldata, "conenumb")),
-          "2" = dispace(vismodeldata),
-          "3" = trispace(vismodeldata),
-          "4" = tcspace(vismodeldata),
-          "seg" = segspace(vismodeldata)
+          "2" = 'di',
+          "3" = 'tri',
+          "4" = 'tcs',
+          "seg" = 'segment'
         )
     }
-  } else {
+  }
+    
+    # Run the model
     res <-
       switch(space2,
         "di" = dispace(vismodeldata),
@@ -132,7 +136,6 @@ colspace <- function(vismodeldata,
         "cielch" = cie(vismodeldata, "LCh"),
         "segment" = segspace(vismodeldata)
       )
-  }
 
   # Include lum for appropriate spaces
   if (!any(space2 %in% c("segment", "ciexyz", "cielab", "cielch"))) {

commit 311bb3074aa833625023d2fadfcaf73f573ff62a

Fix CIE bug introduced in 1a9a8db Also add test to prevent this in the future No need for an item in the release notes since it didn't make it into the stable version

R/colspace.R
@@ -102,8 +102,7 @@ colspace <- function(vismodeldata,
   # Auto-define an appropriate space
   if (space2 == "auto") {
     if (all(c("X", "Y", "Z") %in% names(vismodeldata))) {
-      space2 <- "XYZ"
-      # res <- cie(vismodeldata, "XYZ")
+      space2 <- "ciexyz"
     } else {
       if (is.null(attr(vismodeldata, "conenumb"))) {
         stop(

tests/testthat/test-colspace.R
@@ -160,3 +160,14 @@ test_that("Errors/messages", {
   expect_equivalent(round(sum(summary(colspace(vismodel(flowers)))), 5), 4.08984)
   expect_equivalent(round(sum(summary(colspace(vismodel(flowers))), by = 3), 5), 7.08984)
 })
+
+test_that("CIE", {
+  
+  data(flowers)
+  vis_flowers <- colspace(vismodel(flowers, "cie10"))
+  implicit_cie_flowers <- colspace(vis_flowers)
+  expect_s3_class(implicit_cie_flowers, "colspace")
+  explicit_cie_flowers <- colspace(vis_flowers, space = "ciexyz")
+  expect_identical(implicit_cie_flowers, explicit_cie_flowers)
+  
+})