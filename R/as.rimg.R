e8a7afda5aec720f25ebd09568bb41ed9eaef1b6
Hugo Gruson <hugo.gruson@protonmail.com>
2020-04-20 16:30:23 +0200

    Do not test examples that require imager Since it doesn't seem super stable on CRAN and it doesn't work with sysreqs

--- R/as.rimg.R
+++ R/as.rimg.R
@@ -193,9 +193,11 @@ is.rimg <- function(object) {
 #' @examples
 #' papilio <- getimg(system.file("testdata/images/papilio.png", package = "pavo"))
 #'
+#' \donttest{
 #' # From class rimg to cimg
 #' papilio_cimg <- rimg2cimg(papilio)
 #' class(papilio_cimg)
+#' }
 #'
 #' # From class rimg to magick-image
 #' papilio_magick <- rimg2magick(papilio)
