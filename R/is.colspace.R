866a74d394f168fa5bb650ad7d5416039c8ad149
Hugo Gruson <hugo.gruson@protonmail.com>
2020-04-28 22:40:53 +0200

    Use markup for R in docs

--- R/is.colspace.R
+++ R/is.colspace.R
@@ -1,6 +1,6 @@
 #' Test if object is of class 'colspace'
 #'
-#' @param object an R object
+#' @param object an \R object
 #'
 #' @return a logical value indicating whether the object is of class `colspace`
 #'
--- R/is.vismodel.R
+++ R/is.vismodel.R
@@ -1,6 +1,6 @@
 #' Test if object is of class 'vismodel'
 #'
-#' @param object an R object
+#' @param object an \R object
 #'
 #' @return a logical value indicating whether the object is of class `vismodel`.
 #'
