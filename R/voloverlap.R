90d532d3a121aefcf519bff8f38bc3af11fb19d7
Hugo Gruson <hugo.gruson@protonmail.com>
2020-05-03 10:37:46 +0200

    Add missing $ in reference

--- R/voloverlap.R
+++ R/voloverlap.R
@@ -81,8 +81,8 @@
 #'  Behavioral Ecology, ary017 \doi{10.1093/beheco/ary017}
 #' @references 
 #' Gruson H. 2020. Estimation of colour volumes as concave hypervolumes using 
-#' \ifelse{html}{\out{&alpha;}}{\eqn{$\alpha}{alpha}}‐shapes. Methods in Ecology
-#'  and Evolution, early view \doi{10.1111/2041-210X.13398}
+#'  \ifelse{html}{\out{&alpha;}}{\eqn{$\alpha$}{alpha}}‐shapes. Methods in 
+#'  Ecology and Evolution, early view \doi{10.1111/2041-210X.13398}
 
 voloverlap <- function(colsp1, colsp2, type = c("convex", "alpha"), avalue1,
                        avalue2, plot = FALSE, interactive = FALSE,
