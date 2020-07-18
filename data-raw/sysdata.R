1ae3874024154d3fc953c6570da1f80a08909b95
Hugo Gruson <hugo.gruson@protonmail.com>
2020-07-18 16:22:19 +0200

    Add comments

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,11 +1,21 @@
 library(pavo)
+
+# FIXME: at some point, this line should be removed because all data will be
+# generated from this file and we won't have to rely on existing data anymore.
 load("R/sysdata.rda")
+
+# bgandilum
 
 d65 <- readxl::read_xls("data-raw/ciedata.xls", skip = 5, col_names = c("wl", "d65"), sheet = "D65")
 d65 <- as.rspec(d65, lim = c(300, 700))
 d65 <- procspec(d65, opt = "maximum")
 
 bgandilum$D65 <- d65$d65
+bgandilum$ideal <- 1
+
+# transmissiondata
+
+# vissyst
 
 cie2 <- readxl::read_xls("data-raw/ciedata.xls", range = "1931 col observer!A6:D86", col_names = c("wl", "x", "y", "z"))
 cie2 <- as.rspec(cie2, lim = c(300, 700), exceed.range = FALSE)
