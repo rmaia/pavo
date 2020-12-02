c103b8e29f86ead23e1d65b63e7b42be2f54963d
Hugo Gruson <hugo.gruson@protonmail.com>
2020-12-02 10:59:47 +0100

    Run tests on alphashapes conditionally

--- tests/testthat/test-voloverlap.R
+++ tests/testthat/test-voloverlap.R
@@ -1,34 +1,27 @@
+data(sicalis)
+tcs.sicalis.C <- subset(colspace(vismodel(sicalis)), "C")
+tcs.sicalis.T <- subset(colspace(vismodel(sicalis)), "T")
+tcs.sicalis.B <- subset(colspace(vismodel(sicalis)), "B")
+
 test_that("Overlap", {
-  data(sicalis)
-  tcs.sicalis.C <- subset(colspace(vismodel(sicalis)), "C")
-  tcs.sicalis.T <- subset(colspace(vismodel(sicalis)), "T")
-  tcs.sicalis.B <- subset(colspace(vismodel(sicalis)), "B")
 
   expect_equal(sum(voloverlap(tcs.sicalis.T, tcs.sicalis.B, type = "convex")), 0.1972758, tolerance = 1e-5)
   expect_equal(sum(voloverlap(tcs.sicalis.T, tcs.sicalis.C, type = "convex")), 9.922872e-06, tolerance = 1e-7)
   expect_equal(sum(voloverlap(tcs.sicalis.T, tcs.sicalis.B, type = "convex")[1:2]), 1.146523e-05, tolerance = 1e-6)
 
-  overlap_alpha <- expect_silent(
-    voloverlap(tcs.sicalis.T, tcs.sicalis.B, type = "alpha", avalue = 0.5)
-  )
-
-  expect_equal(overlap_alpha[[1]], 5.183721e-06, tolerance = 1e-7)
-  expect_equal(overlap_alpha[[2]], 6.231493e-06, tolerance = 1e-7)
 })
 
 test_that("tcs", {
-  data(sicalis)
 
   tcs_sicalis <- colspace(vismodel(sicalis))
   vol_sicalis <- voloverlap(tcs_sicalis, tcs_sicalis, type = "convex")
 
   expect_length(vol_sicalis, 5)
   expect_equal(vol_sicalis$vboth, 1)
-  expect_equal(vol_sicalis$vol1, vol_sicalis$vol2)
+  expect_identical(vol_sicalis$vol1, vol_sicalis$vol2)
 })
 
 test_that("tri", {
-  data(sicalis)
 
   tri_sicalis <- colspace(vismodel(sicalis, visual = "ctenophorus"))
   vol_sicalis <- voloverlap(tri_sicalis, tri_sicalis, type = "convex")
@@ -85,9 +78,9 @@ test_that("Symmetric", {
   vol_hq <- voloverlap(hrep, qux, type = "convex")
   vol_qh <- voloverlap(qux, hrep, type = "convex")
 
-  expect_equal(vol_hq$overlapvol, vol_qh$overlapvol)
-  expect_equal(vol_hq$vsmallest, vol_qh$vsmallest)
-  expect_equal(vol_hq$vboth, vol_hq$vboth)
+  expect_identical(vol_hq$overlapvol, vol_qh$overlapvol)
+  expect_identical(vol_hq$vsmallest, vol_qh$vsmallest)
+  expect_identical(vol_hq$vboth, vol_hq$vboth)
 })
 
 test_that("Plane", {
@@ -99,4 +92,17 @@ test_that("Plane", {
   plane_sicalis <- tcs_sicalis[1:3, ]
 
   expect_error(suppressWarnings(voloverlap(plane_sicalis, plane_sicalis, type = "convex"), "error code 1"))
+})
+
+test_that("Alphashapes", {
+
+  skip_if_not_installed("alphashape3d")
+
+  overlap_alpha <- expect_silent(
+    voloverlap(tcs.sicalis.T, tcs.sicalis.B, type = "alpha", avalue = 0.5)
+  )
+
+  expect_equal(overlap_alpha[[1]], 5.183721e-06, tolerance = 1e-7)
+  expect_equal(overlap_alpha[[2]], 6.231493e-06, tolerance = 1e-7)
+
 })
