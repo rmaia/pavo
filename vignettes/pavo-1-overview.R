016965984de2001b23d83424eebe5a7c1a7f8e7c
Thomas White <thomas.white026@gmail.com>
2021-02-23 09:57:01 +1100

    Re-remove vignettes because I screwed something up

--- vignettes/pavo-1-overview.R
+++
@@ -1,3 +0,0 @@
-## ---- echo = FALSE, fig.cap = "A non-exhaustive overview of the colour-pattern analysis workflow in pavo, as of version 2.0, displaying some key functions at each stage.", out.width = '100%', dpi = 72----
-knitr::include_graphics("https://raw.githubusercontent.com/rmaia/pavo/master/vignettes/fig/workflow.png")
-
