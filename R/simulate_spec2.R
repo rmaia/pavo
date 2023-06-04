711f77ccb602b623f0e03084c6ff99afe621859a
Thomas White <thomas.white026@gmail.com>
2023-06-05 08:21:04 +1000

    Use snake case in arguments

--- R/simulate_spec2.R
+++ R/simulate_spec2.R
@@ -4,16 +4,16 @@
 #' (bell-shaped) curves. Multiple Gaussian and sigmoidal curves can be combined in
 #' a single spectrum, to simulate more complex reflectance functions.
 #'
-#' @param wl.inflect A numeric vector specifying the wavelength location (in nm)
+#' @param wl_inflect A numeric vector specifying the wavelength location (in nm)
 #' for one or more inflection point(s) for a 'sigmoid' shaped curve, if desired. 
-#' @param wl.peak A numeric vector specifying the wavelength location (in nm)
+#' @param wl_peak A numeric vector specifying the wavelength location (in nm)
 #' for one or more inflection point(s) for a 'Gaussian' (or 'bell') shaped curve, 
 #' if desired. 
-#' @param width.sig A numeric value or vector (if multiple wl.inflect values are specified) 
+#' @param width_sig A numeric value or vector (if multiple wl_inflect values are specified) 
 #' specifying the steepness of the change, for any sigmoidal curve(s). Required when 
-#' `wl.peak` is specified. Defaults to 20 nm.
-#' @param width.gauss A numeric value or vector specifying the the full-width at half-maximum
-#' of any Gaussian curve(s). Required when `wl.peak` is specified. Defaults to 70 nm.
+#' `wl_peak` is specified. Defaults to 20 nm.
+#' @param width_gauss A numeric value or vector specifying the the full-width at half-maximum
+#' of any Gaussian curve(s). Required when `wl_peak` is specified. Defaults to 70 nm.
 #' @param xlim A vector specifying the wavelength range of the simulated spectra. Defaults to
 #' 300-700nm (`c(300, 700)`).
 #' @param ylim A vector specifying the minimum and maximum reflectance values of the resulting
@@ -23,47 +23,47 @@
 #' 
 #' @examples
 #' # Single sigmoidal spectrum, with an inflection at 500 nm.
-#' spec <- as.rspec(simulate_spec2(wl.inflect = 500))
+#' spec <- as.rspec(simulate_spec2(wl_inflect = 500))
 #'
 #' # Single Gaussian spectrum, with a peak at 400 nm
-#' spec2 <- as.rspec(simulate_spec2(wl.peak = 400))
+#' spec2 <- as.rspec(simulate_spec2(wl_peak = 400))
 #'
 #' # Combination of both Gaussian (with peak at 340 nm) and sigmoidal (with inflection
 #' # at 560 nm)
-#' spec3 <- as.rspec(simulate_spec2(wl.peak = 340, wl.inflect = 560))
+#' spec3 <- as.rspec(simulate_spec2(wl_peak = 340, wl_inflect = 560))
 #'
 #' # Double-Gaussian peaks of differing widths
-#' spec4 <- as.rspec(simulate_spec2(wl.peak = c(340, 560), width.gauss = c(12, 40)))
+#' spec4 <- as.rspec(simulate_spec2(wl_peak = c(340, 560), width_gauss = c(12, 40)))
 #'
 #' # Complex spectrim with multi-Gaussian and single sigmoidal peak
-#' spec5 <- as.rspec(simulate_spec2(wl.peak = c(340, 430), width.gauss = c(20, 60), wl.inflect = 575)) 
+#' spec5 <- as.rspec(simulate_spec2(wl_peak = c(340, 430), width_gauss = c(20, 60), wl_inflect = 575)) 
 #' 
 #' @export
 #' 
 #' @author Thomas White \email{thomas.white026@@gmail.com}
 #' @author Hugo Gruson \email{hugo.gruson+R@@normalesup.org}
 #' 
-simulate_spec2 <- function(wl.inflect = NULL, 
-                           wl.peak = NULL, 
-                           width.sig = 20,
-                           width.gauss = 70,
+simulate_spec2 <- function(wl_inflect = NULL, 
+                           wl_peak = NULL, 
+                           width_sig = 20,
+                           width_gauss = 70,
                            xlim = c(300, 700), 
                            ylim = c(0, 100)) {
   
   # Error handling
-  if (is.null(wl.inflect) && is.null(wl.peak)) {
-    stop("Either wl.inflect or wl.peak must be specified")
+  if (is.null(wl_inflect) && is.null(wl_peak)) {
+    stop("Either wl_inflect or wl_peak must be specified")
   }
   
-  # Check that the size of wl.inflect and width.sig match, or width.sig is a single value
+  # Check that the size of wl_inflect and width_sig match, or width_sig is a single value
   # https://vctrs.r-lib.org/reference/vector_recycling_rules.html
-  if (!is.null(wl.inflect) && length(width.sig) != 1 && length(wl.inflect) != length(width.sig)) {
-    stop("Size of wl.inflect and width.sig must match, or width.sig must be a single value")
+  if (!is.null(wl_inflect) && length(width_sig) != 1 && length(wl_inflect) != length(width_sig)) {
+    stop("Size of wl_inflect and width_sig must match, or width_sig must be a single value")
   }
   
-  # Check that the size of wl.peak and width.gauss match, or width.gauss is a single value
-  if (!is.null(wl.peak) && length(width.gauss) != 1 && length(wl.peak) != length(width.gauss)) {
-    stop("Size of wl.peak and width.gauss must match, or width.gauss must be a single value")
+  # Check that the size of wl_peak and width_gauss match, or width_gauss is a single value
+  if (!is.null(wl_peak) && length(width_gauss) != 1 && length(wl_peak) != length(width_gauss)) {
+    stop("Size of wl_peak and width_gauss must match, or width_gauss must be a single value")
   }
   
   # Generate wavelengths from the specified range
@@ -73,22 +73,22 @@ simulate_spec2 <- function(wl.inflect = NULL,
   spec <- vector("numeric", length(wl))
   
   # Convert FWHM to standard deviation for Gaussian function
-  sigma.gauss <- width.gauss / (2 * sqrt(2 * log(2)))
+  sigma.gauss <- width_gauss / (2 * sqrt(2 * log(2)))
   
   # Simulate the specs
   # Sigmoidal
-  if (!is.null(wl.inflect)) {
+  if (!is.null(wl_inflect)) {
     spec.sig <- mapply(function(wli, wsi) {
       (ylim[2] - ylim[1]) / (1 + exp(-(wl - wli)/wsi)) + ylim[1]
-    }, wl.inflect, width.sig)
+    }, wl_inflect, width_sig)
     spec <- spec + rowSums(spec.sig, na.rm = TRUE)
   }
   
   # Gaussian
-  if (!is.null(wl.peak)) {
+  if (!is.null(wl_peak)) {
     spec.gauss <- mapply(function(wlp, wga) {
       (ylim[2] - ylim[1]) * exp(-((wl - wlp)^2)/(2 * wga^2)) + ylim[1]
-    }, wl.peak, sigma.gauss)
+    }, wl_peak, sigma.gauss)
     spec <- spec + rowSums(spec.gauss, na.rm = TRUE)
   }
   
