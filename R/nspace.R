a9e5d61a326e4886705aa8e77e6537e4299c983f
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-02 18:17:37 +0200

    Run air

--- R/nspace.R
+++ R/nspace.R
@@ -5,35 +5,34 @@
 #'
 #' @param vismodeldata (required) quantum catch color data. Can be either the
 #'   result from [vismodel()] or independently calculated data
-#'   
+#'
 #' @export
-#' 
+#'
 #' @importFrom geometry bary2cart
-#'   
-#' @example 
+#'
+#' @example
 #' fakemantisshrimp <- sensmodel(c(325, 350, 400, 425, 450, 500, 550, 600, 650, 700), beta = FALSE, integrate = FALSE)
-#' 
+#'
 #' data(flowers)
 #' vis_flowers <- vismodel(flowers, visual = fakemantisshrimp)
 #' colsp_flowers <- nspace(vis_flowers)
 
 nspace <- function(vismodeldata) {
-  
   qcatches <- vismodeldata[, colnames(vismodeldata) != "lum"]
   lum <- vismodeldata[, colnames(vismodeldata) == "lum"]
-  ncones <-ncol(qcatches)
+  ncones <- ncol(qcatches)
   message("Detected ", ncones, " cones.")
-  
+
   # Get relative qcatches
   qcatches <- qcatches / rowSums(qcatches)
-  
+
   coords <- bary2cart(simplex(ncones), as.matrix(qcatches))
-  
+
   r.vec <- sqrt(rowSums(apply(coords, 2, function(x) x^2)))
-  
+
   return(data.frame(qcatches, coords, r.vec, lum))
 }
 
 simplex <- function(n) {
-  qr.Q(qr(matrix(1, nrow = n)), complete = TRUE)[,-1]
+  qr.Q(qr(matrix(1, nrow = n)), complete = TRUE)[, -1]
 }
