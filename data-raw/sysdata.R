401be2fabf27b1118cc39a95ec7525d333d11ed0
Hugo Gruson <git@hugogruson.fr>
2026-03-29 17:24:00 +0200

    Make source for "avg.uv" and "avg.v" explicit

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -43,6 +43,25 @@ vissyst[, paste0("cie10_", c("X", "Y", "Z"))] <- cie10[, c("x", "y", "z")]
 dros <- procspec(as.rspec(read.csv(file.path("data-raw", "drosophila_melanogaster_sharkey.csv"))), fixneg = "zero")
 vissyst <- merge(vissyst, dros)
 
+# avg.uv & avg.v come from the online appendix of Endler and Mielke (2005).
+avgs <- read.table(
+  file.path("data-raw", "Endler_Mielke_2005_onlineappendix.txt"),
+  skip = 3,
+  col.names = c(
+    "wl",
+    "avg.uv.u", "avg.uv.s", "avg.uv.m", "avg.uv.l",
+    "avg.v.u", "avg.v.s", "avg.v.m", "avg.v.l"
+  )
+)
+avgs[, -1] <- vapply(avgs[, -1], function(x) x / 100, FUN.VALUE = numeric(nrow(avgs)))
+avgs <- as.rspec(avgs, lim = c(300, 700), interp = FALSE, exceed.range = FALSE)
+vissyst[
+  c(
+    "avg.uv.u", "avg.uv.s", "avg.uv.m", "avg.uv.l",
+    "avg.v.u", "avg.v.s", "avg.v.m", "avg.v.l"
+  )
+] <- avgs[, -1]
+
 # Convert
 
 # Output
